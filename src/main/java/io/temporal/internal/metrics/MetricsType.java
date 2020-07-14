327630f425a9881b2117d7186aba9af2d57a8bb5
Maxim Fateev <mfateev@gmail.com>
2020-07-14 08:17:25 -0700

    Fixed error metric name

--- src/main/java/io/temporal/internal/metrics/MetricsType.java
+++ src/main/java/io/temporal/internal/metrics/MetricsType.java
@@ -59,16 +59,16 @@ public class MetricsType {
       TEMPORAL_METRICS_PREFIX + "activity_execution_latency";
   public static final String ACTIVITY_E2E_LATENCY =
       TEMPORAL_METRICS_PREFIX + "activity_endtoend_latency";
-  public static final String ACTIVITY_TASK_FAILURE_COUNTER =
-      TEMPORAL_METRICS_PREFIX + "activity_task_failure";
+  public static final String ACTIVITY_TASK_ERROR_COUNTER =
+      TEMPORAL_METRICS_PREFIX + "activity_task_error_thrown";
   public static final String LOCAL_ACTIVITY_TOTAL_COUNTER =
       TEMPORAL_METRICS_PREFIX + "local_activity_total";
   public static final String LOCAL_ACTIVITY_CANCELED_COUNTER =
       TEMPORAL_METRICS_PREFIX + "local_activity_canceled";
   public static final String LOCAL_ACTIVITY_FAILED_COUNTER =
       TEMPORAL_METRICS_PREFIX + "local_activity_failed";
-  public static final String LOCAL_ACTIVITY_FAILURE_COUNTER =
-      TEMPORAL_METRICS_PREFIX + "local_activity_panic";
+  public static final String LOCAL_ACTIVITY_ERROR_COUNTER =
+      TEMPORAL_METRICS_PREFIX + "local_activity_error_thrown";
   public static final String LOCAL_ACTIVITY_EXECUTION_LATENCY =
       TEMPORAL_METRICS_PREFIX + "local_activity_execution_latency";
   public static final String CORRUPTED_SIGNALS_COUNTER =
