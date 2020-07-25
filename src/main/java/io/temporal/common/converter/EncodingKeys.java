46b69cf9fe6ac0db52d8f992e9f0fb290db4ac99
Manu Srivastava <manu@temporal.io>
2020-07-25 09:45:37 -0700

    uploads payload header values to align with server / tctl

--- src/main/java/io/temporal/common/converter/EncodingKeys.java
+++ src/main/java/io/temporal/common/converter/EncodingKeys.java
@@ -24,23 +24,24 @@ import java.nio.charset.StandardCharsets;
 
 class EncodingKeys {
   static final String METADATA_ENCODING_KEY = "encoding";
-  static final String METADATA_ENCODING_NULL_NAME = "null";
+
+  static final String METADATA_ENCODING_NULL_NAME = "binary/null";
   static final ByteString METADATA_ENCODING_NULL =
       ByteString.copyFrom(METADATA_ENCODING_NULL_NAME, StandardCharsets.UTF_8);
 
-  static final String METADATA_ENCODING_RAW_NAME = "raw";
+  static final String METADATA_ENCODING_RAW_NAME = "binary/plain";
   static final ByteString METADATA_ENCODING_RAW =
       ByteString.copyFrom(METADATA_ENCODING_RAW_NAME, StandardCharsets.UTF_8);
 
-  static final String METADATA_ENCODING_JSON_NAME = "json";
+  static final String METADATA_ENCODING_JSON_NAME = "json/plain";
   static final ByteString METADATA_ENCODING_JSON =
       ByteString.copyFrom(METADATA_ENCODING_JSON_NAME, StandardCharsets.UTF_8);
 
-  static final String METADATA_ENCODING_PROTOBUF_JSON_NAME = "protobuf-json";
+  static final String METADATA_ENCODING_PROTOBUF_JSON_NAME = "json/protobuf";
   static final ByteString METADATA_ENCODING_PROTOBUF_JSON =
       ByteString.copyFrom(METADATA_ENCODING_PROTOBUF_JSON_NAME, StandardCharsets.UTF_8);
 
-  static final String METADATA_ENCODING_PROTOBUF_NAME = "protobuf";
+  static final String METADATA_ENCODING_PROTOBUF_NAME = "binary/protobuf";
   static final ByteString METADATA_ENCODING_PROTOBUF =
       ByteString.copyFrom(METADATA_ENCODING_PROTOBUF_NAME, StandardCharsets.UTF_8);
 }
