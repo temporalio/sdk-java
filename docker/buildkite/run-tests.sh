e7519304b532f3bbbfa9f0f7296e4d878c693b47
Vitaly <Vitaly.Arbuzov@gmail.com>
2021-01-06 20:06:05 -0800

    Move run tests into a separate script

---
+++ docker/buildkite/run-tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+./gradlew --no-daemon test
+
+if [ ! -z "$(git status --porcelain)" ]; then 
+	echo 'Some files were improperly formatted. Please run build locally and check in all changes.'
+	git status;
+	exit 1;
+fi
+
