9e26a984dd22f28cdb2fbf4f69f84dba79511dd4
Vitaly <Vitaly.Arbuzov@gmail.com>
2021-01-07 17:18:32 -0800

    Enforce formatting on buildkite (#293)

---
+++ docker/buildkite/run-tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -eou pipefail
+set -x
+
+./gradlew --no-daemon test
+
+if [ ! -z "$(git status --porcelain)" ]; then 
+	echo 'Some files were improperly formatted. Please run build locally and check in all changes.'
+	git status;
+	exit 1;
+fi
+
