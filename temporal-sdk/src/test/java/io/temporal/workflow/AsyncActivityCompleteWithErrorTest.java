6884dfe6e2341dcc794a660f89ac0adc50c880d0
Vitaly <Vitaly.Arbuzov@gmail.com>
2021-01-13 23:46:54 -0800

    apply formatting

--- temporal-sdk/src/test/java/io/temporal/workflow/AsyncActivityCompleteWithErrorTest.java
+++ temporal-sdk/src/test/java/io/temporal/workflow/AsyncActivityCompleteWithErrorTest.java
@@ -25,85 +25,84 @@ import io.temporal.client.WorkflowOptions;
 import io.temporal.common.RetryOptions;
 import io.temporal.failure.ApplicationFailure;
 import io.temporal.testing.TestWorkflowRule;
+import java.time.Duration;
+import java.util.concurrent.ForkJoinPool;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.time.Duration;
-import java.util.concurrent.ForkJoinPool;
-
 public class AsyncActivityCompleteWithErrorTest {
 
-    @Rule
-    public TestWorkflowRule testWorkflowRule =
-            TestWorkflowRule.newBuilder()
-                    .setWorkflowTypes(TestWorkflowImpl.class)
-                    .setActivityImplementations(new AsyncActivityWithManualCompletion())
-                    .setUseExternalService(Boolean.parseBoolean(System.getenv("USE_DOCKER_SERVICE")))
-                    .setTarget(System.getenv("TEMPORAL_SERVICE_ADDRESS"))
-                    .build();
+  @Rule
+  public TestWorkflowRule testWorkflowRule =
+      TestWorkflowRule.newBuilder()
+          .setWorkflowTypes(TestWorkflowImpl.class)
+          .setActivityImplementations(new AsyncActivityWithManualCompletion())
+          .setUseExternalService(Boolean.parseBoolean(System.getenv("USE_DOCKER_SERVICE")))
+          .setTarget(System.getenv("TEMPORAL_SERVICE_ADDRESS"))
+          .build();
 
-    @WorkflowInterface
-    public interface TestWorkflow {
+  @WorkflowInterface
+  public interface TestWorkflow {
 
-        @WorkflowMethod
-        String execute(String taskQueue);
+    @WorkflowMethod
+    String execute(String taskQueue);
+  }
+
+  public static class TestWorkflowImpl implements TestWorkflow {
+
+    @Override
+    public String execute(String taskQueue) {
+      TestActivity activity =
+          Workflow.newActivityStub(
+              TestActivity.class,
+              ActivityOptions.newBuilder()
+                  .setScheduleToStartTimeout(Duration.ofSeconds(1))
+                  .setScheduleToCloseTimeout(Duration.ofSeconds(1))
+                  .setRetryOptions(RetryOptions.newBuilder().setMaximumAttempts(1).build())
+                  .build());
+      Promise<Integer> promise = Async.function(activity::execute);
+      RuntimeException failure = promise.getFailure();
+      Assert.assertNotNull(failure);
+      Assert.assertTrue(failure.getCause() instanceof ApplicationFailure);
+      Assert.assertEquals(
+          "simulated failure", ((ApplicationFailure) failure.getCause()).getOriginalMessage());
+      return "success";
+    }
+  }
+
+  @ActivityInterface
+  public interface TestActivity {
+
+    @ActivityMethod
+    int execute();
+  }
+
+  public static class AsyncActivityWithManualCompletion implements TestActivity {
+    @Override
+    public int execute() {
+      ActivityExecutionContext context = Activity.getExecutionContext();
+      ActivityCompletionClient completionClient = context.useLocalManualCompletion();
+      ForkJoinPool.commonPool().execute(() -> asyncActivityFn(context, completionClient));
+      return 0;
     }
 
-    public static class TestWorkflowImpl implements TestWorkflow {
-
-        @Override
-        public String execute(String taskQueue) {
-            TestActivity activity =
-                    Workflow.newActivityStub(
-                            TestActivity.class,
-                            ActivityOptions.newBuilder()
-                                    .setScheduleToStartTimeout(Duration.ofSeconds(1))
-                                    .setScheduleToCloseTimeout(Duration.ofSeconds(1))
-                                    .setRetryOptions(RetryOptions.newBuilder().setMaximumAttempts(1).build())
-                                    .build());
-            Promise<Integer> promise = Async.function(activity::execute);
-            RuntimeException failure = promise.getFailure();
-            Assert.assertNotNull(failure);
-            Assert.assertTrue(failure.getCause() instanceof ApplicationFailure);
-            Assert.assertEquals(
-                    "simulated failure", ((ApplicationFailure) failure.getCause()).getOriginalMessage());
-            return "success";
-        }
+    private void asyncActivityFn(
+        ActivityExecutionContext context, ActivityCompletionClient completionClient) {
+      completionClient.completeExceptionally(
+          context.getTaskToken(), ApplicationFailure.newFailure("simulated failure", "test"));
     }
+  }
 
-    @ActivityInterface
-    public interface TestActivity {
-
-        @ActivityMethod
-        int execute();
-    }
-
-    public static class AsyncActivityWithManualCompletion implements TestActivity {
-        @Override
-        public int execute() {
-            ActivityExecutionContext context = Activity.getExecutionContext();
-            ActivityCompletionClient completionClient = context.useLocalManualCompletion();
-            ForkJoinPool.commonPool().execute(() -> asyncActivityFn(context, completionClient));
-            return 0;
-        }
-
-        private void asyncActivityFn(
-                ActivityExecutionContext context, ActivityCompletionClient completionClient) {
-            completionClient.completeExceptionally(
-                    context.getTaskToken(), ApplicationFailure.newFailure("simulated failure", "test"));
-        }
-    }
-
-    @Test
-    public void verifyActivityCompletionClientCompleteExceptionally() {
-        String taskQueue = testWorkflowRule.getTaskQueue();
-        TestWorkflow workflow =
-                testWorkflowRule
-                        .getWorkflowClient()
-                        .newWorkflowStub(
-                                TestWorkflow.class, WorkflowOptions.newBuilder().setTaskQueue(taskQueue).build());
-        String result = workflow.execute(taskQueue);
-        Assert.assertEquals("success", result);
-    }
+  @Test
+  public void verifyActivityCompletionClientCompleteExceptionally() {
+    String taskQueue = testWorkflowRule.getTaskQueue();
+    TestWorkflow workflow =
+        testWorkflowRule
+            .getWorkflowClient()
+            .newWorkflowStub(
+                TestWorkflow.class, WorkflowOptions.newBuilder().setTaskQueue(taskQueue).build());
+    String result = workflow.execute(taskQueue);
+    Assert.assertEquals("success", result);
+  }
 }
