488081be3d171b67306b4ac2b19d107f9d8f7cca
Maxim Fateev <mfateev@gmail.com>
2021-01-16 14:24:24 -0800

    Added processSignal back

--- temporal-sdk/src/main/java/io/temporal/common/interceptors/WorkflowInboundCallsInterceptor.java
+++ temporal-sdk/src/main/java/io/temporal/common/interceptors/WorkflowInboundCallsInterceptor.java
@@ -95,4 +95,7 @@ public interface WorkflowInboundCallsInterceptor {
    * @return result of the workflow execution.
    */
   WorkflowOutput execute(WorkflowInput input);
+
+  /** Called when signal is delivered to the workflow instance. */
+  void processSignal(SignalInput input);
 }
