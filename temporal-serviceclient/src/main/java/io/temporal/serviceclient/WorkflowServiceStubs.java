7015980bc92f26ade27c74ee9a1cd3a390d06b5e
Maxim Fateev <mfateev@gmail.com>
2021-02-09 12:39:09 -0800

    Added javadoc to awaitTermination

--- temporal-serviceclient/src/main/java/io/temporal/serviceclient/WorkflowServiceStubs.java
+++ temporal-serviceclient/src/main/java/io/temporal/serviceclient/WorkflowServiceStubs.java
@@ -20,6 +20,7 @@
 package io.temporal.serviceclient;
 
 import io.temporal.api.workflowservice.v1.WorkflowServiceGrpc;
+
 import java.util.concurrent.TimeUnit;
 
 /** Initializes and holds gRPC blocking and future stubs. */
@@ -61,6 +62,12 @@ public interface WorkflowServiceStubs {
 
   boolean isTerminated();
 
+  /**
+   * Awaits for gRPC stubs shutdown up to the specified timeout. The shutdown has to be initiated
+   * through {@link #shutdown()} or {@link #shutdownNow()}.
+   *
+   * @return false if timed out or the thread was interrupted.
+   */
   boolean awaitTermination(long timeout, TimeUnit unit);
 
   WorkflowServiceStubsOptions getOptions();
