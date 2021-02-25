dedb9b3b2f0ec7b3a3583a94bdcc8994d60ea82a
Vera Kobylchak <vera@temporal.io>
2021-02-24 16:15:06 -0800

    Added another QueryDispatcher test, updated Gradle and dependencies.

--- temporal-sdk/src/test/java/io/temporal/internal/replay/ReplayWorkflowRunTaskHandlerTaskHandlerTests.java
+++ temporal-sdk/src/test/java/io/temporal/internal/replay/ReplayWorkflowRunTaskHandlerTaskHandlerTests.java
@@ -23,7 +23,7 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
--- temporal-sdk/src/test/java/io/temporal/internal/sync/QueryDispatcherTest.java
+++ temporal-sdk/src/test/java/io/temporal/internal/sync/QueryDispatcherTest.java
@@ -19,26 +19,65 @@
 
 package io.temporal.internal.sync;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import io.temporal.api.common.v1.Payloads;
+import io.temporal.common.converter.DataConverter;
+import io.temporal.common.interceptors.WorkflowInboundCallsInterceptor;
 import io.temporal.common.interceptors.WorkflowOutboundCallsInterceptor;
+import java.lang.reflect.Type;
+import java.util.Optional;
 import org.junit.Test;
 
 public class QueryDispatcherTest {
 
+  private QueryDispatcher initializeDispatcher() {
+    // Initialize the dispatcher.
+    QueryDispatcher dispatcher = new QueryDispatcher(DataConverter.getDefaultInstance());
+    WorkflowOutboundCallsInterceptor.RegisterQueryInput requestA =
+        new WorkflowOutboundCallsInterceptor.RegisterQueryInput(
+            "QueryA", new Class[] {}, new Type[] {}, null);
+    WorkflowOutboundCallsInterceptor.RegisterQueryInput requestB =
+        new WorkflowOutboundCallsInterceptor.RegisterQueryInput(
+            "QueryB", new Class[] {}, new Type[] {}, null);
+    dispatcher.registerQueryHandlers(requestA);
+    dispatcher.registerQueryHandlers(requestB);
+
+    return dispatcher;
+  }
+
+  @Test
+  public void testQuerySuccess() {
+    QueryDispatcher dispatcher = initializeDispatcher();
+
+    // Set up a mock interceptor.
+    WorkflowInboundCallsInterceptor.QueryOutput queryOutput =
+        new WorkflowInboundCallsInterceptor.QueryOutput("dummy");
+    WorkflowInboundCallsInterceptor mockInterceptor = mock(WorkflowInboundCallsInterceptor.class);
+    when(mockInterceptor.handleQuery(any())).thenReturn(queryOutput);
+
+    // Initialize the dispatcher.
+    dispatcher.setInboundCallsInterceptor(mockInterceptor);
+
+    // Invoke functionality under test, expect no exceptions for an existing query.
+    Optional<Payloads> queryResult = dispatcher.handleQuery("QueryB", Optional.empty());
+    assertTrue(queryResult.isPresent());
+  }
+
   @Test
   public void testQueryDispatcherException() {
-    QueryDispatcher qd = new QueryDispatcher(null);
-    WorkflowOutboundCallsInterceptor.RegisterQueryInput requestA =
-        new WorkflowOutboundCallsInterceptor.RegisterQueryInput("QueryA", null, null, null);
-    qd.registerQueryHandlers(requestA);
+    QueryDispatcher dispatcher = initializeDispatcher();
+    // Invoke functionality under test, expect an exception with the correct output for a
+    // non-existing query.
     Throwable exception =
         assertThrows(
             IllegalArgumentException.class,
             () -> {
-              qd.handleQuery("Query", null);
+              dispatcher.handleQuery("QueryC", null);
             });
-    assertEquals("Unknown query type: Query, knownTypes=[QueryA]", exception.getMessage());
+    assertEquals("Unknown query type: QueryC, knownTypes=[QueryA, QueryB]", exception.getMessage());
   }
 }
