91f8ea082c8b0e3da657052380ba056be0ab5e64
Vera Kobylchak <vera@temporal.io>
2021-03-18 15:27:01 -0700

    These tests fail at the moment. Pushing to PR to get hints

---
+++ temporal-sdk/src/test/java/io/temporal/workflow/SyncTest.java
@@ -0,0 +1,137 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.workflow;
+
+import static io.temporal.client.WorkflowClient.QUERY_TYPE_STACK_TRACE;
+import static org.junit.Assert.*;
+
+import io.temporal.api.common.v1.WorkflowExecution;
+import io.temporal.client.WorkflowFailedException;
+import io.temporal.client.WorkflowStub;
+import io.temporal.failure.CanceledFailure;
+import io.temporal.failure.TerminatedFailure;
+import io.temporal.testing.TracingWorkerInterceptor;
+import io.temporal.workflow.shared.SDKTestWorkflowRule;
+import io.temporal.workflow.shared.TestActivities;
+import io.temporal.workflow.shared.TestOptions;
+import io.temporal.workflow.shared.TestWorkflows;
+import java.time.Duration;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class SyncTest {
+
+  private final TestActivities.TestActivitiesImpl activitiesImpl =
+      new TestActivities.TestActivitiesImpl(null);
+
+  @Rule
+  public SDKTestWorkflowRule testWorkflowRule =
+      SDKTestWorkflowRule.newBuilder()
+          .setWorkflowTypes(TestSyncWorkflowImpl.class)
+          .setActivityImplementations(activitiesImpl)
+          .setWorkerInterceptors(
+              new TracingWorkerInterceptor(new TracingWorkerInterceptor.FilteredTrace()))
+          .build();
+
+  @Test
+  public void testSync() {
+    TestWorkflows.TestWorkflow1 workflowStub =
+        testWorkflowRule.newWorkflowStubTimeoutOptions(TestWorkflows.TestWorkflow1.class);
+    String result = workflowStub.execute(testWorkflowRule.getTaskQueue());
+    Assert.assertEquals("activity10", result);
+    testWorkflowRule
+        .getInterceptor(TracingWorkerInterceptor.class)
+        .setExpected(
+            "interceptExecuteWorkflow " + SDKTestWorkflowRule.UUID_REGEXP,
+            "newThread workflow-method",
+            "newThread null",
+            "sleep PT2S",
+            "executeActivity ActivityWithDelay",
+            "activity ActivityWithDelay",
+            "executeActivity Activity2",
+            "activity Activity2");
+  }
+
+  @Test
+  public void testSyncUntypedAndStackTrace() {
+    WorkflowStub workflowStub =
+        testWorkflowRule.newUntypedWorkflowStubTimeoutOptions("TestWorkflow1");
+    WorkflowExecution execution = workflowStub.start(testWorkflowRule.getTaskQueue());
+    testWorkflowRule.sleep(Duration.ofMillis(500));
+    String stackTrace = workflowStub.query(QUERY_TYPE_STACK_TRACE, String.class);
+    assertTrue(stackTrace, stackTrace.contains("WorkflowTest$TestSyncWorkflowImpl.execute"));
+    assertTrue(stackTrace, stackTrace.contains("activityWithDelay"));
+    // Test stub created from workflow execution.
+    workflowStub =
+        testWorkflowRule
+            .getWorkflowClient()
+            .newUntypedWorkflowStub(execution, workflowStub.getWorkflowType());
+    stackTrace = workflowStub.query(QUERY_TYPE_STACK_TRACE, String.class);
+    assertTrue(stackTrace, stackTrace.contains("WorkflowTest$TestSyncWorkflowImpl.execute"));
+    assertTrue(stackTrace, stackTrace.contains("activityWithDelay"));
+    String result = workflowStub.getResult(String.class);
+    assertEquals("activity10", result);
+  }
+
+  @Test
+  public void testWorkflowCancellation() {
+    WorkflowStub client = testWorkflowRule.newUntypedWorkflowStubTimeoutOptions("TestWorkflow1");
+    client.start(testWorkflowRule.getTaskQueue());
+    client.cancel();
+    try {
+      client.getResult(String.class);
+      fail("unreachable");
+    } catch (WorkflowFailedException e) {
+      assertTrue(e.getCause() instanceof CanceledFailure);
+    }
+  }
+
+  @Test
+  public void testWorkflowTermination() throws InterruptedException {
+    WorkflowStub client = testWorkflowRule.newUntypedWorkflowStubTimeoutOptions("TestWorkflow1");
+    client.start(testWorkflowRule.getTaskQueue());
+    Thread.sleep(1000);
+    client.terminate("boo", "detail1", "detail2");
+    try {
+      client.getResult(String.class);
+      fail("unreachable");
+    } catch (WorkflowFailedException ignored) {
+      assertTrue(ignored.getCause() instanceof TerminatedFailure);
+      assertEquals("boo", ((TerminatedFailure) ignored.getCause()).getOriginalMessage());
+    }
+  }
+
+  public static class TestSyncWorkflowImpl implements TestWorkflows.TestWorkflow1 {
+
+    @Override
+    public String execute(String taskQueue) {
+      TestActivities activities =
+          Workflow.newActivityStub(
+              TestActivities.class, TestOptions.newActivityOptionsForTaskQueue(taskQueue));
+      // Invoke synchronously in a separate thread for testing purposes only.
+      // In real workflows use
+      // Async.procedure(activities::activityWithDelay, 1000, true)
+      Promise<String> a1 = Async.function(() -> activities.activityWithDelay(1000, true));
+      Workflow.sleep(2000);
+      return activities.activity2(a1.get(), 10);
+    }
+  }
+}
---
+++ temporal-sdk/src/test/java/io/temporal/workflow/versionTests/GetVersionWithoutCommandEventTest.java
@@ -0,0 +1,95 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.workflow.versionTests;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowStub;
+import io.temporal.workflow.CompletablePromise;
+import io.temporal.workflow.Workflow;
+import io.temporal.workflow.WorkflowTest;
+import io.temporal.workflow.shared.SDKTestWorkflowRule;
+import io.temporal.workflow.shared.TestActivities;
+import java.util.concurrent.CompletableFuture;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class GetVersionWithoutCommandEventTest {
+
+  private static CompletableFuture<Boolean> executionStarted = new CompletableFuture<>();
+  private final TestActivities.TestActivitiesImpl activitiesImpl =
+      new TestActivities.TestActivitiesImpl(null);
+
+  @Rule
+  public SDKTestWorkflowRule testWorkflowRule =
+      SDKTestWorkflowRule.newBuilder()
+          .setWorkflowTypes(TestGetVersionWithoutCommandEventWorkflowImpl.class)
+          .setActivityImplementations(activitiesImpl)
+          .setTestTimeoutSeconds(20)
+          .build();
+
+  @Test
+  public void testGetVersionWithoutCommandEvent() throws Exception {
+    executionStarted = new CompletableFuture<Boolean>();
+    WorkflowTest.TestWorkflowSignaled workflowStub =
+        testWorkflowRule.newWorkflowStubTimeoutOptions(WorkflowTest.TestWorkflowSignaled.class);
+    WorkflowClient.start(workflowStub::execute);
+    executionStarted.get();
+    workflowStub.signal1("test signal");
+    String result = WorkflowStub.fromTyped(workflowStub).getResult(String.class);
+    Assert.assertEquals("result 1", result);
+  }
+
+  public static class TestGetVersionWithoutCommandEventWorkflowImpl
+      implements WorkflowTest.TestWorkflowSignaled {
+
+    CompletablePromise<Boolean> signalReceived = Workflow.newPromise();
+
+    @Override
+    public String execute() {
+      try {
+        if (!Workflow.isReplaying()) {
+          executionStarted.complete(true);
+          signalReceived.get();
+        } else {
+          // Execute getVersion in replay mode. In this case we have no command event, only a
+          // signal.
+          int version = Workflow.getVersion("test_change", Workflow.DEFAULT_VERSION, 1);
+          if (version == Workflow.DEFAULT_VERSION) {
+            signalReceived.get();
+            return "result 1";
+          } else {
+            return "result 2";
+          }
+        }
+        Workflow.sleep(1000);
+      } catch (Exception e) {
+        throw new RuntimeException("failed to get from signal");
+      }
+
+      throw new RuntimeException("unreachable");
+    }
+
+    @Override
+    public void signal1(String arg) {
+      signalReceived.complete(true);
+    }
+  }
+}
