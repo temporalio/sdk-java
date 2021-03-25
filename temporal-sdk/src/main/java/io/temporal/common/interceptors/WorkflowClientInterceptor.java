temporalio/sdk-java

commit fd6bb90ac1f53815bf88de3563db7f6177b29a76

Add WorkflowStubCallsInterceptor which provides an access to Headers (#382)

temporal-sdk/src/main/java/io/temporal/common/interceptors/WorkflowClientInterceptor.java
@@ -23,15 +23,47 @@ import io.temporal.api.common.v1.WorkflowExecution;
 import io.temporal.client.ActivityCompletionClient;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.client.WorkflowStub;
+import io.temporal.common.Experimental;
 import java.util.Optional;
 
+@Experimental
 public interface WorkflowClientInterceptor {
-
+  /**
+   * Called when workflow stub is instantiated during creation of new workflow. It allows to
+   * decorate calls to {@link WorkflowStub} instance which is an entry point for client code.
+   *
+   * @return decorated stub
+   * @deprecated consider implementing all intercepting functionality using {@link
+   *     WorkflowClientCallsInterceptor} that is produced in {@link
+   *     #workflowClientClassInterceptor}. This method has to stay temporary because
+   *     TimeLockingInterceptor has to intercept top level {@link WorkflowStub} methods.
+   */
+  @Deprecated
   WorkflowStub newUntypedWorkflowStub(
       String workflowType, WorkflowOptions options, WorkflowStub next);
 
+  /**
+   * Called when workflow stub is instantiated for a known existing workflow execution. It allows to
+   * decorate calls to {@link WorkflowStub} instance which is an entry point for client code.
+   *
+   * @return decorated stub
+   * @deprecated consider implementing all intercepting functionality using {@link
+   *     WorkflowClientCallsInterceptor} that is produced in {@link
+   *     #workflowClientClassInterceptor}. This method has to stay temporary because
+   *     TimeLockingInterceptor has to intercept top level {@link WorkflowStub} methods.
+   */
+  @Deprecated
   WorkflowStub newUntypedWorkflowStub(
       WorkflowExecution execution, Optional<String> workflowType, WorkflowStub next);
 
   ActivityCompletionClient newActivityCompletionClient(ActivityCompletionClient next);
+
+  /**
+   * Called once during creation of WorkflowClient to create a chain of Client Interceptors
+   *
+   * @param next next workflow client interceptor in the chain of interceptors
+   * @return new interceptor that should decorate calls to {@code next}
+   */
+  WorkflowClientCallsInterceptor workflowClientClassInterceptor(
+      WorkflowClientCallsInterceptor next);
 }

temporal-sdk/src/main/java/io/temporal/common/interceptors/WorkflowClientInterceptorBase.java
@@ -28,12 +28,14 @@ import java.util.Optional;
 /** Convenience base class for WorkflowClientInterceptor implementations. */
 public class WorkflowClientInterceptorBase implements WorkflowClientInterceptor {
 
+  @Deprecated
   @Override
   public WorkflowStub newUntypedWorkflowStub(
       String workflowType, WorkflowOptions options, WorkflowStub next) {
     return next;
   }
 
+  @Deprecated
   @Override
   public WorkflowStub newUntypedWorkflowStub(
       WorkflowExecution execution, Optional<String> workflowType, WorkflowStub next) {
@@ -42,6 +44,12 @@ public class WorkflowClientInterceptorBase implements WorkflowClientInterceptor 
 
   @Override
   public ActivityCompletionClient newActivityCompletionClient(ActivityCompletionClient next) {
+    return next;
+  }
+
+  @Override
+  public WorkflowClientCallsInterceptor workflowClientClassInterceptor(
+      WorkflowClientCallsInterceptor next) {
     return next;
   }
 }

temporal-sdk/src/main/java/io/temporal/internal/sync/WorkflowClientInternal.java
@@ -30,7 +30,9 @@ import io.temporal.client.WorkflowClientOptions;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.client.WorkflowStub;
 import io.temporal.common.converter.DataConverter;
+import io.temporal.common.interceptors.WorkflowClientCallsInterceptor;
 import io.temporal.common.interceptors.WorkflowClientInterceptor;
+import io.temporal.internal.client.RootWorkflowClientInvoker;
 import io.temporal.internal.external.GenericWorkflowClientExternalImpl;
 import io.temporal.internal.external.ManualActivityCompletionClientFactory;
 import io.temporal.internal.external.ManualActivityCompletionClientFactoryImpl;
@@ -55,6 +57,7 @@ public final class WorkflowClientInternal implements WorkflowClient {
   private final ManualActivityCompletionClientFactory manualActivityCompletionClientFactory;
   private final DataConverter dataConverter;
   private final WorkflowClientInterceptor[] interceptors;
+  private final WorkflowClientCallsInterceptor workflowClientCallsInvoker;
   private final WorkflowServiceStubs workflowServiceStubs;
   private final Scope metricsScope;
 
@@ -87,9 +90,20 @@ public final class WorkflowClientInternal implements WorkflowClient {
             workflowServiceStubs, options.getNamespace(), options.getIdentity(), metricsScope);
     this.dataConverter = options.getDataConverter();
     this.interceptors = options.getInterceptors();
+    this.workflowClientCallsInvoker = initializeClientInvoker();
     this.manualActivityCompletionClientFactory =
         new ManualActivityCompletionClientFactoryImpl(
             workflowServiceStubs, options.getNamespace(), dataConverter, metricsScope);
+  }
+
+  private WorkflowClientCallsInterceptor initializeClientInvoker() {
+    WorkflowClientCallsInterceptor workflowClientInvoker =
+        new RootWorkflowClientInvoker(genericClient, options, metricsScope);
+    for (WorkflowClientInterceptor clientInterceptor : interceptors) {
+      workflowClientInvoker =
+          clientInterceptor.workflowClientClassInterceptor(workflowClientInvoker);
+    }
+    return workflowClientInvoker;
   }
 
   @Override
@@ -108,7 +122,7 @@ public final class WorkflowClientInternal implements WorkflowClient {
     checkAnnotation(workflowInterface, WorkflowMethod.class);
     WorkflowInvocationHandler invocationHandler =
         new WorkflowInvocationHandler(
-            workflowInterface, this.getOptions(), genericClient, options, metricsScope);
+            workflowInterface, this.getOptions(), workflowClientCallsInvoker, options);
     return (T)
         Proxy.newProxyInstance(
             workflowInterface.getClassLoader(),
@@ -157,7 +171,7 @@ public final class WorkflowClientInternal implements WorkflowClient {
 
     WorkflowInvocationHandler invocationHandler =
         new WorkflowInvocationHandler(
-            workflowInterface, this.getOptions(), genericClient, execution, metricsScope);
+            workflowInterface, this.getOptions(), workflowClientCallsInvoker, execution);
     @SuppressWarnings("unchecked")
     T result =
         (T)
@@ -169,9 +183,10 @@ public final class WorkflowClientInternal implements WorkflowClient {
   }
 
   @Override
+  @SuppressWarnings("deprecation")
   public WorkflowStub newUntypedWorkflowStub(String workflowType, WorkflowOptions workflowOptions) {
     WorkflowStub result =
-        new WorkflowStubImpl(options, genericClient, workflowType, workflowOptions, metricsScope);
+        new WorkflowStubImpl(options, workflowClientCallsInvoker, workflowType, workflowOptions);
     for (WorkflowClientInterceptor i : interceptors) {
       result = i.newUntypedWorkflowStub(workflowType, workflowOptions, result);
     }
@@ -189,7 +204,7 @@ public final class WorkflowClientInternal implements WorkflowClient {
   @Override
   public WorkflowStub newUntypedWorkflowStub(
       WorkflowExecution execution, Optional<String> workflowType) {
-    return new WorkflowStubImpl(options, genericClient, workflowType, execution, metricsScope);
+    return new WorkflowStubImpl(options, workflowClientCallsInvoker, workflowType, execution);
   }
 
   @Override

temporal-sdk/src/test/java/io/temporal/workflow/ChildWorkflowRetryTest.java
@@ -29,6 +29,8 @@ import io.temporal.client.WorkflowFailedException;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.client.WorkflowStub;
 import io.temporal.common.RetryOptions;
+import io.temporal.common.interceptors.WorkflowClientCallsInterceptor;
+import io.temporal.common.interceptors.WorkflowClientCallsInterceptorBase;
 import io.temporal.common.interceptors.WorkflowClientInterceptorBase;
 import io.temporal.failure.ApplicationFailure;
 import io.temporal.failure.ChildWorkflowFailure;
@@ -65,10 +67,23 @@ public class ChildWorkflowRetryTest {
                   .setInterceptors(
                       new WorkflowClientInterceptorBase() {
                         @Override
-                        public WorkflowStub newUntypedWorkflowStub(
-                            String workflowType, WorkflowOptions options, WorkflowStub next) {
-                          lastStartedWorkflowType.set(workflowType);
-                          return next;
+                        public WorkflowClientCallsInterceptor workflowClientClassInterceptor(
+                            WorkflowClientCallsInterceptor next) {
+                          return new WorkflowClientCallsInterceptorBase(next) {
+                            @Override
+                            public WorkflowStartOutput start(WorkflowStartInput input) {
+                              lastStartedWorkflowType.set(input.getWorkflowType());
+                              return super.start(input);
+                            }
+
+                            @Override
+                            public WorkflowSignalWithStartOutput signalWithStart(
+                                WorkflowSignalWithStartInput input) {
+                              lastStartedWorkflowType.set(
+                                  input.getWorkflowStartInput().getWorkflowType());
+                              return super.signalWithStart(input);
+                            }
+                          };
                         }
                       })
                   .setNamespace(NAMESPACE)

temporal-sdk/src/test/java/io/temporal/workflow/WorkflowTest.java
@@ -40,6 +40,8 @@ import io.temporal.common.MethodRetry;
 import io.temporal.common.RetryOptions;
 import io.temporal.common.converter.DataConverter;
 import io.temporal.common.converter.GsonJsonPayloadConverter;
+import io.temporal.common.interceptors.WorkflowClientCallsInterceptor;
+import io.temporal.common.interceptors.WorkflowClientCallsInterceptorBase;
 import io.temporal.common.interceptors.WorkflowClientInterceptorBase;
 import io.temporal.failure.*;
 import io.temporal.internal.common.SearchAttributesUtil;
@@ -140,10 +142,23 @@ public class WorkflowTest {
             .setInterceptors(
                 new WorkflowClientInterceptorBase() {
                   @Override
-                  public WorkflowStub newUntypedWorkflowStub(
-                      String workflowType, WorkflowOptions options, WorkflowStub next) {
-                    lastStartedWorkflowType.set(workflowType);
-                    return next;
+                  public WorkflowClientCallsInterceptor workflowClientClassInterceptor(
+                      WorkflowClientCallsInterceptor next) {
+                    return new WorkflowClientCallsInterceptorBase(next) {
+                      @Override
+                      public WorkflowStartOutput start(WorkflowStartInput input) {
+                        lastStartedWorkflowType.set(input.getWorkflowType());
+                        return super.start(input);
+                      }
+
+                      @Override
+                      public WorkflowSignalWithStartOutput signalWithStart(
+                          WorkflowSignalWithStartInput input) {
+                        lastStartedWorkflowType.set(
+                            input.getWorkflowStartInput().getWorkflowType());
+                        return super.signalWithStart(input);
+                      }
+                    };
                   }
                 })
             .setNamespace(SDKTestWorkflowRule.NAMESPACE)

temporal-sdk/src/main/java/io/temporal/common/Experimental.java
@@ -1,0 +1,31 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.common;
+
+import java.lang.annotation.*;
+
+/**
+ * Annotation that specifies that an element is experimental, has unstable API or may change without
+ * notice. This annotation is inherited.
+ */
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE})
+public @interface Experimental {}

temporal-sdk/src/main/java/io/temporal/common/interceptors/WorkflowClientCallsInterceptor.java
@@ -1,0 +1,361 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.common.interceptors;
+
+import io.temporal.api.common.v1.WorkflowExecution;
+import io.temporal.api.enums.v1.WorkflowExecutionStatus;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.common.Experimental;
+import java.lang.reflect.Type;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Experimental
+public interface WorkflowClientCallsInterceptor {
+  /**
+   * @see #signalWithStart if you implement this method, {@link #signalWithStart} most likely needs
+   *     to be implemented too
+   */
+  WorkflowStartOutput start(WorkflowStartInput input);
+
+  /**
+   * @see #signalWithStart if you implement this method, {@link #signalWithStart} most likely needs
+   *     to be implemented too
+   */
+  WorkflowSignalOutput signal(WorkflowSignalInput input);
+
+  WorkflowSignalWithStartOutput signalWithStart(WorkflowSignalWithStartInput input);
+
+  /**
+   * @see #getResultAsync if you implement this method, {@link #getResultAsync} most likely needs to
+   *     be implemented too
+   */
+  <R> GetResultOutput<R> getResult(GetResultInput<R> input) throws TimeoutException;
+
+  /**
+   * @see #getResult if you implement this method, {@link #getResult} most likely needs to be
+   *     implemented too
+   */
+  <R> GetResultAsyncOutput<R> getResultAsync(GetResultInput<R> input);
+
+  <R> QueryOutput<R> query(QueryInput<R> input);
+
+  CancelOutput cancel(CancelInput input);
+
+  TerminateOutput terminate(TerminateInput input);
+
+  final class WorkflowStartInput {
+    private final String workflowId;
+    private final String workflowType;
+    private final Header header;
+    private final Object[] arguments;
+    private final WorkflowOptions options;
+
+    public WorkflowStartInput(
+        String workflowId,
+        String workflowType,
+        Header header,
+        Object[] arguments,
+        WorkflowOptions options) {
+      this.workflowId = workflowId;
+      this.workflowType = workflowType;
+      this.header = header;
+      this.arguments = arguments;
+      this.options = options;
+    }
+
+    public String getWorkflowId() {
+      return workflowId;
+    }
+
+    public String getWorkflowType() {
+      return workflowType;
+    }
+
+    public Header getHeader() {
+      return header;
+    }
+
+    public Object[] getArguments() {
+      return arguments;
+    }
+
+    public WorkflowOptions getOptions() {
+      return options;
+    }
+  }
+
+  final class WorkflowStartOutput {
+    private final WorkflowExecution workflowExecution;
+
+    public WorkflowStartOutput(WorkflowExecution workflowExecution) {
+      this.workflowExecution = workflowExecution;
+    }
+
+    public WorkflowExecution getWorkflowExecution() {
+      return workflowExecution;
+    }
+  }
+
+  final class WorkflowSignalInput {
+    private final WorkflowExecution workflowExecution;
+    private final String signalName;
+    private final Object[] arguments;
+
+    public WorkflowSignalInput(
+        WorkflowExecution workflowExecution, String signalName, Object[] signalArguments) {
+      this.workflowExecution = workflowExecution;
+      this.signalName = signalName;
+      this.arguments = signalArguments;
+    }
+
+    public WorkflowExecution getWorkflowExecution() {
+      return workflowExecution;
+    }
+
+    public String getSignalName() {
+      return signalName;
+    }
+
+    public Object[] getArguments() {
+      return arguments;
+    }
+  }
+
+  final class WorkflowSignalOutput {}
+
+  final class WorkflowSignalWithStartInput {
+    private final WorkflowStartInput workflowStartInput;
+    private final String signalName;
+    private final Object[] signalArguments;
+
+    public WorkflowSignalWithStartInput(
+        WorkflowStartInput workflowStartInput, String signalName, Object[] signalArguments) {
+      this.workflowStartInput = workflowStartInput;
+      this.signalName = signalName;
+      this.signalArguments = signalArguments;
+    }
+
+    public WorkflowStartInput getWorkflowStartInput() {
+      return workflowStartInput;
+    }
+
+    public String getSignalName() {
+      return signalName;
+    }
+
+    public Object[] getSignalArguments() {
+      return signalArguments;
+    }
+  }
+
+  final class WorkflowSignalWithStartOutput {
+    private final WorkflowStartOutput workflowStartOutput;
+
+    public WorkflowSignalWithStartOutput(WorkflowStartOutput workflowStartOutput) {
+      this.workflowStartOutput = workflowStartOutput;
+    }
+
+    public WorkflowStartOutput getWorkflowStartOutput() {
+      return workflowStartOutput;
+    }
+  }
+
+  final class GetResultInput<R> {
+    private final WorkflowExecution workflowExecution;
+    private final Optional<String> workflowType;
+    private final long timeout;
+    private final TimeUnit timeoutUnit;
+    private final Class<R> resultClass;
+    private final Type resultType;
+
+    public GetResultInput(
+        WorkflowExecution workflowExecution,
+        Optional<String> workflowType,
+        long timeout,
+        TimeUnit timeoutUnit,
+        Class<R> resultClass,
+        Type resultType) {
+      this.workflowExecution = workflowExecution;
+      this.workflowType = workflowType;
+      this.timeout = timeout;
+      this.timeoutUnit = timeoutUnit;
+      this.resultClass = resultClass;
+      this.resultType = resultType;
+    }
+
+    public WorkflowExecution getWorkflowExecution() {
+      return workflowExecution;
+    }
+
+    public Optional<String> getWorkflowType() {
+      return workflowType;
+    }
+
+    public long getTimeout() {
+      return timeout;
+    }
+
+    public TimeUnit getTimeoutUnit() {
+      return timeoutUnit;
+    }
+
+    public Class<R> getResultClass() {
+      return resultClass;
+    }
+
+    public Type getResultType() {
+      return resultType;
+    }
+  }
+
+  final class GetResultOutput<R> {
+    private final R result;
+
+    public GetResultOutput(R result) {
+      this.result = result;
+    }
+
+    public R getResult() {
+      return result;
+    }
+  }
+
+  final class GetResultAsyncOutput<R> {
+    private final CompletableFuture<R> result;
+
+    public GetResultAsyncOutput(CompletableFuture<R> result) {
+      this.result = result;
+    }
+
+    public CompletableFuture<R> getResult() {
+      return result;
+    }
+  }
+
+  final class QueryInput<R> {
+    private final WorkflowExecution workflowExecution;
+    private final String queryType;
+    private final Object[] arguments;
+    private final Class<R> resultClass;
+    private final Type resultType;
+
+    public QueryInput(
+        WorkflowExecution workflowExecution,
+        String queryType,
+        Object[] arguments,
+        Class<R> resultClass,
+        Type resultType) {
+      this.workflowExecution = workflowExecution;
+      this.queryType = queryType;
+      this.arguments = arguments;
+      this.resultClass = resultClass;
+      this.resultType = resultType;
+    }
+
+    public WorkflowExecution getWorkflowExecution() {
+      return workflowExecution;
+    }
+
+    public String getQueryType() {
+      return queryType;
+    }
+
+    public Object[] getArguments() {
+      return arguments;
+    }
+
+    public Class<R> getResultClass() {
+      return resultClass;
+    }
+
+    public Type getResultType() {
+      return resultType;
+    }
+  }
+
+  final class QueryOutput<R> {
+    private final WorkflowExecutionStatus queryRejectedStatus;
+    private final R result;
+
+    /**
+     * @param queryRejectedStatus should be null if query is not rejected
+     * @param result converted result value
+     */
+    public QueryOutput(WorkflowExecutionStatus queryRejectedStatus, R result) {
+      this.queryRejectedStatus = queryRejectedStatus;
+      this.result = result;
+    }
+
+    public boolean isQueryRejected() {
+      return queryRejectedStatus != null;
+    }
+
+    public WorkflowExecutionStatus getQueryRejectedStatus() {
+      return queryRejectedStatus;
+    }
+
+    public R getResult() {
+      return result;
+    }
+  }
+
+  final class CancelInput {
+    private final WorkflowExecution workflowExecution;
+
+    public CancelInput(WorkflowExecution workflowExecution) {
+      this.workflowExecution = workflowExecution;
+    }
+
+    public WorkflowExecution getWorkflowExecution() {
+      return workflowExecution;
+    }
+  }
+
+  final class CancelOutput {}
+
+  final class TerminateInput {
+    private final WorkflowExecution workflowExecution;
+    private final String reason;
+    private final Object[] details;
+
+    public TerminateInput(WorkflowExecution workflowExecution, String reason, Object[] details) {
+      this.workflowExecution = workflowExecution;
+      this.reason = reason;
+      this.details = details;
+    }
+
+    public WorkflowExecution getWorkflowExecution() {
+      return workflowExecution;
+    }
+
+    public String getReason() {
+      return reason;
+    }
+
+    public Object[] getDetails() {
+      return details;
+    }
+  }
+
+  final class TerminateOutput {}
+}

commit 3dfe345d61baada82e78d130d56dc3b16bf82735

Fix a typo in workflowClientCallsInterceptor() method name introduced in #382

temporal-sdk/src/main/java/io/temporal/common/interceptors/WorkflowClientInterceptor.java
@@ -35,7 +35,7 @@ public interface WorkflowClientInterceptor {
    * @return decorated stub
    * @deprecated consider implementing all intercepting functionality using {@link
    *     WorkflowClientCallsInterceptor} that is produced in {@link
-   *     #workflowClientClassInterceptor}. This method has to stay temporary because
+   *     #workflowClientCallsInterceptor}. This method has to stay temporary because
    *     TimeLockingInterceptor has to intercept top level {@link WorkflowStub} methods.
    */
   @Deprecated
@@ -49,7 +49,7 @@ public interface WorkflowClientInterceptor {
    * @return decorated stub
    * @deprecated consider implementing all intercepting functionality using {@link
    *     WorkflowClientCallsInterceptor} that is produced in {@link
-   *     #workflowClientClassInterceptor}. This method has to stay temporary because
+   *     #workflowClientCallsInterceptor}. This method has to stay temporary because
    *     TimeLockingInterceptor has to intercept top level {@link WorkflowStub} methods.
    */
   @Deprecated
@@ -59,11 +59,11 @@ public interface WorkflowClientInterceptor {
   ActivityCompletionClient newActivityCompletionClient(ActivityCompletionClient next);
 
   /**
-   * Called once during creation of WorkflowClient to create a chain of Client Interceptors
+   * Called once during creation of WorkflowClient to create a chain of Client Workflow Interceptors
    *
    * @param next next workflow client interceptor in the chain of interceptors
    * @return new interceptor that should decorate calls to {@code next}
    */
-  WorkflowClientCallsInterceptor workflowClientClassInterceptor(
+  WorkflowClientCallsInterceptor workflowClientCallsInterceptor(
       WorkflowClientCallsInterceptor next);
 }

temporal-sdk/src/main/java/io/temporal/common/interceptors/WorkflowClientInterceptorBase.java
@@ -48,7 +48,7 @@ public class WorkflowClientInterceptorBase implements WorkflowClientInterceptor 
   }
 
   @Override
-  public WorkflowClientCallsInterceptor workflowClientClassInterceptor(
+  public WorkflowClientCallsInterceptor workflowClientCallsInterceptor(
       WorkflowClientCallsInterceptor next) {
     return next;
   }

temporal-sdk/src/main/java/io/temporal/internal/sync/WorkflowClientInternal.java
@@ -101,7 +101,7 @@ public final class WorkflowClientInternal implements WorkflowClient {
         new RootWorkflowClientInvoker(genericClient, options, metricsScope);
     for (WorkflowClientInterceptor clientInterceptor : interceptors) {
       workflowClientInvoker =
-          clientInterceptor.workflowClientClassInterceptor(workflowClientInvoker);
+          clientInterceptor.workflowClientCallsInterceptor(workflowClientInvoker);
     }
     return workflowClientInvoker;
   }

temporal-sdk/src/main/java/io/temporal/internal/sync/WorkflowStubImpl.java
@@ -384,8 +384,8 @@ class WorkflowStubImpl implements WorkflowStub {
   }
 
   /**
-   * RunId can change e.g. workflow does ContinueAsNew.
-   * Emptying runId in workflowExecution allows Temporal server figure out the current run id dynamically.
+   * RunId can change e.g. workflow does ContinueAsNew. Emptying runId in workflowExecution allows
+   * Temporal server figure out the current run id dynamically.
    */
   private WorkflowExecution currentExecutionWithoutRunId() {
     WorkflowExecution workflowExecution = execution.get();

temporal-sdk/src/test/java/io/temporal/workflow/ChildWorkflowRetryTest.java
@@ -67,7 +67,7 @@ public class ChildWorkflowRetryTest {
                   .setInterceptors(
                       new WorkflowClientInterceptorBase() {
                         @Override
-                        public WorkflowClientCallsInterceptor workflowClientClassInterceptor(
+                        public WorkflowClientCallsInterceptor workflowClientCallsInterceptor(
                             WorkflowClientCallsInterceptor next) {
                           return new WorkflowClientCallsInterceptorBase(next) {
                             @Override

temporal-sdk/src/test/java/io/temporal/workflow/WorkflowTest.java
@@ -142,7 +142,7 @@ public class WorkflowTest {
             .setInterceptors(
                 new WorkflowClientInterceptorBase() {
                   @Override
-                  public WorkflowClientCallsInterceptor workflowClientClassInterceptor(
+                  public WorkflowClientCallsInterceptor workflowClientCallsInterceptor(
                       WorkflowClientCallsInterceptor next) {
                     return new WorkflowClientCallsInterceptorBase(next) {
                       @Override