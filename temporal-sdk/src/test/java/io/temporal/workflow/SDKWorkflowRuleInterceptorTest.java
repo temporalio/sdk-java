3f087b6c37b1577b6a6dee666b8d492037c21cd4
Vera Kobylchak <vera@temporal.io>
2021-04-01 15:51:12 -0700

    Added a WorkflowRuleInterceptorTest

---
+++ temporal-sdk/src/test/java/io/temporal/workflow/SDKWorkflowRuleInterceptorTest.java
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.workflow;
+
+import io.temporal.testing.TracingWorkerInterceptor;
+import io.temporal.worker.WorkerFactoryOptions;
+import io.temporal.workflow.shared.SDKTestWorkflowRule;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class SDKWorkflowRuleInterceptorTest {
+
+  @Rule public SDKTestWorkflowRule testWorkflowRule1 = SDKTestWorkflowRule.newBuilder().build();
+
+  @Test
+  public void testWorkerInterceptorWorkerFactoryOptionsNotSet() {
+    Assert.assertNotNull(testWorkflowRule1.getInterceptor(TracingWorkerInterceptor.class));
+  }
+
+  @Rule
+  public SDKTestWorkflowRule testWorkflowRule2 =
+      SDKTestWorkflowRule.newBuilder()
+          .setWorkerFactoryOptions(WorkerFactoryOptions.getDefaultInstance())
+          .build();
+
+  @Test
+  public void testWorkerInterceptorWorkerFactoryOptionsSet() {
+    Assert.assertNotNull(testWorkflowRule1.getInterceptor(TracingWorkerInterceptor.class));
+  }
+
+  @Rule
+  public SDKTestWorkflowRule testWorkflowRule3 =
+      SDKTestWorkflowRule.newBuilder()
+          .setWorkerFactoryOptions(
+              WorkerFactoryOptions.newBuilder()
+                  .setWorkerInterceptors(
+                      new TracingWorkerInterceptor(new TracingWorkerInterceptor.FilteredTrace()))
+                  .build())
+          .build();
+
+  @Test
+  public void testWorkerInterceptorWorkerFactoryOptionsSetWithInterceptor() {
+    Assert.assertNotNull(testWorkflowRule1.getInterceptor(TracingWorkerInterceptor.class));
+  }
+}
