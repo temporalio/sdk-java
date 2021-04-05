d9a0c1559c4ec5b6ed009f4a27a541ae0f8b7a1d
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2021-04-05 14:06:11 -0400

    OpenTracing module is implemented (#418)

---
+++ temporal-opentracing/src/main/java/io/temporal/opentracing/OpenTracingWorkerInterceptor.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.opentracing;
+
+import io.temporal.common.interceptors.*;
+import io.temporal.opentracing.internal.OpenTracingActivityInboundCallsInterceptor;
+import io.temporal.opentracing.internal.OpenTracingWorkflowInboundCallsInterceptor;
+import io.temporal.opentracing.internal.SpanFactory;
+
+public class OpenTracingWorkerInterceptor implements WorkerInterceptor {
+  private final OpenTracingOptions options;
+
+  public OpenTracingWorkerInterceptor() {
+    this(OpenTracingOptions.getDefaultInstance());
+  }
+
+  public OpenTracingWorkerInterceptor(OpenTracingOptions options) {
+    this.options = options;
+  }
+
+  @Override
+  public WorkflowInboundCallsInterceptor interceptWorkflow(WorkflowInboundCallsInterceptor next) {
+    return new OpenTracingWorkflowInboundCallsInterceptor(next, options, new SpanFactory(options));
+  }
+
+  @Override
+  public ActivityInboundCallsInterceptor interceptActivity(ActivityInboundCallsInterceptor next) {
+    return new OpenTracingActivityInboundCallsInterceptor(next, options, new SpanFactory(options));
+  }
+}
