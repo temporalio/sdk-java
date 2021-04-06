1383ca9b68f8522d9240bd6aebe9f01f57319d6d
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2021-04-06 17:01:59 -0400

    Fix a potential NullPointer in exception processing inside WorkflowStubImpl
    Address the problem with WorkflowStubImpl#startWithOptions and WorkflowStubImpl#signalWithStartWithOptions passing workflowExecution that could be not initialized into NotNull workflowExecution param of WorkflowServiceException constructor

--- temporal-sdk/src/main/java/io/temporal/client/WorkflowServiceException.java
+++ temporal-sdk/src/main/java/io/temporal/client/WorkflowServiceException.java
@@ -20,10 +20,11 @@
 package io.temporal.client;
 
 import io.temporal.api.common.v1.WorkflowExecution;
+import javax.annotation.Nonnull;
 
 public class WorkflowServiceException extends WorkflowException {
   public WorkflowServiceException(
-      WorkflowExecution execution, String workflowType, Throwable cause) {
+      @Nonnull WorkflowExecution execution, String workflowType, Throwable cause) {
     super(execution, workflowType, cause);
   }
 }
