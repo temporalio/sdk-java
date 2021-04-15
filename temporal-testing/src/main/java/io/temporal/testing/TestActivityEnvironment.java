12fa99fd139b856512f11d338dcad39a9e3fb365
Vera Kobylchak <vera@temporal.io>
2021-04-15 17:22:11 -0500

    Added documentation

--- temporal-testing/src/main/java/io/temporal/testing/TestActivityEnvironment.java
+++ temporal-testing/src/main/java/io/temporal/testing/TestActivityEnvironment.java
@@ -88,7 +88,9 @@ public interface TestActivityEnvironment {
    * @param <T> Type of the activity interface.
    * @param activityInterface activity interface class that the object under test implements
    * @param options options that specify the activity invocation parameters
-   * @param activityMethodOptions activity method-specific invocation parameters
+   * @param activityMethodOptions a map keyed on activityType.name to its specific invocation
+   *     parameters. By default the name of an activity type is its method name with the first
+   *     letter capitalized.
    * @return The stub that implements the activity interface.
    */
   <T> T newActivityStub(
@@ -103,7 +105,9 @@ public interface TestActivityEnvironment {
    * @param <T> Type of the activity interface.
    * @param activityInterface activity interface class that the object under test implements
    * @param options options that specify the activity invocation parameters
-   * @param activityMethodOptions activity method-specific invocation parameters
+   * @param activityMethodOptions a map keyed on activityType.name to its specific invocation
+   *     parameters. By default the name of an activity type is its method name with the first
+   *     letter capitalized.
    * @return The stub that implements the activity interface.
    */
   <T> T newLocalActivityStub(
