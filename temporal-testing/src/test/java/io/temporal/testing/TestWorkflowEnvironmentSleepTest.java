6ca94cb637af412c185ad6489ec17a871a3eb946
Maxim Fateev <mfateev@gmail.com>
2021-05-01 13:00:39 -0700

    Fixed time skipping for workflow task scheduling

--- temporal-testing/src/test/java/io/temporal/testing/TestWorkflowEnvironmentSleepTest.java
+++ temporal-testing/src/test/java/io/temporal/testing/TestWorkflowEnvironmentSleepTest.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
 package io.temporal.testing;
 
 import io.temporal.client.WorkflowClient;
@@ -10,9 +29,24 @@ import io.temporal.workflow.WorkflowMethod;
 import java.time.Duration;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 
 public class TestWorkflowEnvironmentSleepTest {
+
+  @Rule
+  public TestWatcher watchman =
+      new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+          if (testEnv != null) {
+            System.err.println(testEnv.getDiagnostics());
+            testEnv.close();
+          }
+        }
+      };
 
   @WorkflowInterface
   public interface ExampleWorkflow {
@@ -20,7 +54,7 @@ public class TestWorkflowEnvironmentSleepTest {
     void execute();
 
     @SignalMethod
-    void updateState(String state);
+    void signal();
   }
 
   public static class ExampleWorkflowImpl implements ExampleWorkflow {
@@ -30,7 +64,7 @@ public class TestWorkflowEnvironmentSleepTest {
     }
 
     @Override
-    public void updateState(String newState) {}
+    public void signal() {}
   }
 
   private TestWorkflowEnvironment testEnv;
@@ -53,10 +87,9 @@ public class TestWorkflowEnvironmentSleepTest {
   }
 
   @Test(timeout = 2000)
-  public void testCompletedThenTimerCompletes() throws InterruptedException {
+  public void testSignalAfterStartThenSleep() {
     WorkflowClient.start(workflow::execute);
-    // commenting out signal makes the test pass
-    workflow.updateState("PRELIM_COMPLETED");
+    workflow.signal();
     testEnv.sleep(Duration.ofMinutes(50L));
   }
 
