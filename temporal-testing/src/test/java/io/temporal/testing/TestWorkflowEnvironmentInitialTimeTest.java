b9dd6d9be9545e4f0efc056375636023b6461235
Mykola Rybak <mykola.rybak@gmail.com>
2021-05-19 23:23:37 +0300

    Option to set initial time for workflow test (#498)
    * Option to set initial time for workflow test
    
    Add new initialTime attribute to the TestEnvironmentOptions and expose
    it via TestWorkflowRule and TestWorkflowExtension. If set, that
    attribute specifies the initial time to use for the workflow virtual
    clock.
    
    The current behaviour to use current system time as the initial workflow
    time is kept as a default.
    
    * Drop default values for longs

---
+++ temporal-testing/src/test/java/io/temporal/testing/TestWorkflowEnvironmentInitialTimeTest.java
@@ -0,0 +1,104 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.testing;
+
+import static org.junit.Assert.assertEquals;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.worker.Worker;
+import io.temporal.workflow.Workflow;
+import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+public class TestWorkflowEnvironmentInitialTimeTest {
+
+  private static final Instant WORKFLOW_START_TIME = Instant.parse("2020-01-01T00:00:00Z");
+  private static final Duration WORKFLOW_DURATION = Duration.ofHours(10);
+  private static final Instant WORKFLOW_END_TIME = Instant.parse("2020-01-01T10:00:00Z");
+
+  @Rule
+  public TestWatcher watchman =
+      new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+          if (testEnv != null) {
+            System.err.println(testEnv.getDiagnostics());
+            testEnv.close();
+          }
+        }
+      };
+
+  @WorkflowInterface
+  public interface TimeDependentWorkflow {
+    @WorkflowMethod
+    Instant waitAndCheckTime(Duration waitDuration);
+  }
+
+  public static class TimeDependentWorkflowImpl implements TimeDependentWorkflow {
+    @Override
+    public Instant waitAndCheckTime(Duration waitDuration) {
+      Workflow.sleep(waitDuration);
+      return Instant.ofEpochMilli(Workflow.currentTimeMillis());
+    }
+  }
+
+  private TestWorkflowEnvironment testEnv;
+  private TimeDependentWorkflow workflow;
+  private static final String WORKFLOW_TASK_QUEUE = "EXAMPLE";
+
+  @Before
+  public void setUp() {
+
+    TestEnvironmentOptions testEnvOptions =
+        TestEnvironmentOptions.newBuilder().setInitialTime(WORKFLOW_START_TIME).build();
+    testEnv = TestWorkflowEnvironment.newInstance(testEnvOptions);
+
+    Worker worker = testEnv.newWorker(WORKFLOW_TASK_QUEUE);
+    worker.registerWorkflowImplementationTypes(TimeDependentWorkflowImpl.class);
+
+    WorkflowClient client = testEnv.getWorkflowClient();
+    workflow =
+        client.newWorkflowStub(
+            TimeDependentWorkflow.class,
+            WorkflowOptions.newBuilder().setTaskQueue(WORKFLOW_TASK_QUEUE).build());
+    testEnv.start();
+  }
+
+  @Test(timeout = 2000)
+  public void testSignalAfterStartThenSleep() {
+    Instant workflowEndTime = workflow.waitAndCheckTime(WORKFLOW_DURATION);
+    assertEquals(WORKFLOW_END_TIME, workflowEndTime.truncatedTo(ChronoUnit.HOURS));
+  }
+
+  @After
+  public void tearDown() {
+    testEnv.close();
+  }
+}
