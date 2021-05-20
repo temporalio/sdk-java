877ce9687a47ba9f56c9f33cd9dd7fca66aeb339
Vera Kobylchak <vera@temporal.io>
2021-05-19 18:01:56 -0700

    Typo

--- temporal-sdk/src/main/java/io/temporal/internal/sync/SyncWorkflowContext.java
+++ temporal-sdk/src/main/java/io/temporal/internal/sync/SyncWorkflowContext.java
@@ -172,7 +172,7 @@ final class SyncWorkflowContext implements WorkflowOutboundCallsInterceptor {
   public void setActivityOptions(Map<String, ActivityOptions> activityMethodOptions) {
     Objects.requireNonNull(activityMethodOptions);
     if (this.activityOptionsMap == null) {
-      new HashMap<>(activityMethodOptions);
+      this.activityOptionsMap = new HashMap<>(activityMethodOptions);
       return;
     }
     activityMethodOptions.forEach(
