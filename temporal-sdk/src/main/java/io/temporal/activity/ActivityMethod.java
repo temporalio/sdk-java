2e7aa2facf207f77df9d6c5149b50bddaf05abe7
Jay Ong <shingjay.ong@gmail.com>
2021-06-03 12:59:00 -0700

    Cleaning up Activity Javadocs (#453)
    * Cleaning up Activity* documentation
    
    * Address review feedback
    
    * Address additional review comments
    
    * More review comments
    
    * Update temporal-sdk/src/main/java/io/temporal/activity/ActivityCancellationType.java
    
    Co-authored-by: Cully <cullywakelin@gmail.com>
    
    * Thread local comment
    
    * Update temporal-sdk/src/main/java/io/temporal/activity/ActivityCancellationType.java
    
    Co-authored-by: Spencer Judge <sjudge@hey.com>
    
    Co-authored-by: Cully <cullywakelin@gmail.com>
    Co-authored-by: Cully <cully@temporal.io>
    Co-authored-by: Spencer Judge <sjudge@hey.com>

--- temporal-sdk/src/main/java/io/temporal/activity/ActivityMethod.java
+++ temporal-sdk/src/main/java/io/temporal/activity/ActivityMethod.java
@@ -25,19 +25,20 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that the method is an activity method. This annotation applies only to activity
- * interface methods. Not required. Use it to override default activity type name.
+ * Indicates that the method is an Activity method. This annotation applies only to Activity
+ * interface methods. Use it to override default Activity type name. Not required.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface ActivityMethod {
 
   /**
-   * Name of the activity type. Default is method name. Also consider using {@link
-   * ActivityInterface#namePrefix()}. Note that the prefix is ignored if the name is specified.
+   * Represents the name of the Activity type. Default value is the method's name, with the first
+   * letter capitalized. Also consider using {@link ActivityInterface#namePrefix}. The prefix is
+   * ignored if the name is specified.
    *
-   * <p>Be careful about names that contain special characters. These names can be used as metric
-   * tags. And systems like prometheus ignore metrics which have tags with unsupported characters.
+   * <p>Be careful with names that contain special characters, as these names can be used as metric
+   * tags. Systems like Prometheus ignore metrics which have tags with unsupported characters.
    */
   String name() default "";
 }
