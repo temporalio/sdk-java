temporalio/java-sdk

Fixed an issue with time skipping in case of timing out a root (not a child) workflow Issue #577

temporal-testing/src/main/java/io/temporal/internal/testservice/SelfAdvancingTimerImpl.java
@@ -23,10 +23,7 @@ import com.google.common.util.concurrent.Uninterruptibles;
 import io.temporal.workflow.Functions;
 import java.sql.Timestamp;
 import java.time.Duration;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
@@ -97,7 +94,13 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
       while (!Thread.currentThread().isInterrupted()) {
         updateTimeLocked();
         if (!emptyQueue && tasks.isEmpty()) {
-          lockTimeSkippingLocked("runLocked"); // Switching to wall time when no tasks scheduled
+          if (timeLockOnEmptyQueueHandle != null) {
+            throw new IllegalStateException(
+                "SelfAdvancingTimerImpl should have a taken time lock when queue is not empty, but handle is not null");
+          }
+          // Switching to wall time when no tasks scheduled
+          timeLockOnEmptyQueueHandle =
+              lockTimeSkippingLocked("SelfAdvancingTimerImpl runLocked empty-queue");
           emptyQueue = true;
         }
         TimerTask peekedTask = tasks.peek();
@@ -181,6 +184,11 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
 
     @Override
     public void unlock() {
+      unlock(null);
+    }
+
+    @Override
+    public void unlock(String caller) {
       lock.lock();
       try {
         unlockFromHandleLocked();
@@ -191,7 +199,7 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
     }
 
     private void unlockFromHandleLocked() {
-      Boolean removed = lockEvents.remove(event);
+      boolean removed = lockEvents.remove(event);
       if (!removed) {
         throw new IllegalStateException("Unbalanced lock and unlock calls");
       }
@@ -225,12 +233,13 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
   private final PriorityQueue<TimerTask> tasks =
       new PriorityQueue<>(Comparator.comparing(TimerTask::getExecutionTime));
   private final Thread timerPump = new Thread(new TimerPump(), "SelfAdvancingTimer Pump");
+  private LockHandle timeLockOnEmptyQueueHandle;
 
   public SelfAdvancingTimerImpl(long initialTime) {
     currentTime = initialTime == 0 ? System.currentTimeMillis() : initialTime;
     executor.setRejectedExecutionHandler(new CallerRunsPolicy());
     // Queue is initially empty. The code assumes that in this case skipping is already locked.
-    lockTimeSkipping("SelfAdvancingTimerImpl constructor");
+    timeLockOnEmptyQueueHandle = lockTimeSkipping("SelfAdvancingTimerImpl constructor empty-queue");
     timerPump.start();
   }
 
@@ -277,7 +286,13 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
       tasks.add(timerTask);
       // Locked when queue became empty
       if (tasks.size() == 1 && emptyQueue) {
-        unlockTimeSkippingLocked("schedule task for " + taskInfo);
+        if (timeLockOnEmptyQueueHandle == null) {
+          throw new IllegalStateException(
+              "SelfAdvancingTimerImpl should take a lock and get a handle when queue is empty, but handle is null");
+        }
+        timeLockOnEmptyQueueHandle.unlock(
+            "SelfAdvancingTimerImpl schedule non-empty-queue, task: " + taskInfo);
+        timeLockOnEmptyQueueHandle = null;
         emptyQueue = false;
       }
       condition.signal();

temporal-testing/src/test/java/io/temporal/testing/TestWorkflowEnvironmentSleepTest.java
@@ -19,8 +19,14 @@
 
 package io.temporal.testing;
 
+import static org.junit.Assert.*;
+
+import io.temporal.api.enums.v1.TimeoutType;
 import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowFailedException;
 import io.temporal.client.WorkflowOptions;
+import io.temporal.client.WorkflowStub;
+import io.temporal.failure.TimeoutFailure;
 import io.temporal.worker.Worker;
 import io.temporal.workflow.SignalMethod;
 import io.temporal.workflow.Workflow;
@@ -57,7 +63,7 @@ public class TestWorkflowEnvironmentSleepTest {
     void signal();
   }
 
-  public static class ExampleWorkflowImpl implements ExampleWorkflow {
+  public static class HingingWorkflowWithSignalImpl implements ExampleWorkflow {
     @Override
     public void execute() {
       Workflow.sleep(Duration.ofMinutes(20));
@@ -70,7 +76,6 @@ public class TestWorkflowEnvironmentSleepTest {
   private TestWorkflowEnvironment testEnv;
   private Worker worker;
   private WorkflowClient client;
-  private ExampleWorkflow workflow;
   private static final String WORKFLOW_TASK_QUEUE = "EXAMPLE";
 
   @Before
@@ -78,23 +83,49 @@ public class TestWorkflowEnvironmentSleepTest {
     testEnv = TestWorkflowEnvironment.newInstance();
     worker = testEnv.newWorker(WORKFLOW_TASK_QUEUE);
     client = testEnv.getWorkflowClient();
-    worker.registerWorkflowImplementationTypes(ExampleWorkflowImpl.class);
-    workflow =
-        client.newWorkflowStub(
-            ExampleWorkflow.class,
-            WorkflowOptions.newBuilder().setTaskQueue(WORKFLOW_TASK_QUEUE).build());
+    worker.registerWorkflowImplementationTypes(HingingWorkflowWithSignalImpl.class);
     testEnv.start();
-  }
-
-  @Test(timeout = 2000)
-  public void testSignalAfterStartThenSleep() {
-    WorkflowClient.start(workflow::execute);
-    workflow.signal();
-    testEnv.sleep(Duration.ofMinutes(50L));
   }
 
   @After
   public void tearDown() {
     testEnv.close();
+  }
+
+  @Test(timeout = 2000)
+  public void testSignalAfterStartThenSleep() {
+    ExampleWorkflow workflow =
+        client.newWorkflowStub(
+            ExampleWorkflow.class,
+            WorkflowOptions.newBuilder().setTaskQueue(WORKFLOW_TASK_QUEUE).build());
+    WorkflowClient.start(workflow::execute);
+    workflow.signal();
+    testEnv.sleep(Duration.ofMinutes(50L));
+  }
+
+  @Test
+  public void testWorkflowTimeoutDuringSleep() {
+    ExampleWorkflow workflow =
+        client.newWorkflowStub(
+            ExampleWorkflow.class,
+            WorkflowOptions.newBuilder()
+                .setWorkflowExecutionTimeout(Duration.ofMinutes(3))
+                .setTaskQueue(WORKFLOW_TASK_QUEUE)
+                .build());
+
+    WorkflowClient.start(workflow::execute);
+
+    testEnv.sleep(Duration.ofMinutes(11L));
+
+    WorkflowStub workflowStub = WorkflowStub.fromTyped(workflow);
+    try {
+      workflowStub.getResult(Void.class);
+      fail("Workflow should fail with timeout exception");
+    } catch (WorkflowFailedException e) {
+      Throwable cause = e.getCause();
+      assertTrue(cause instanceof TimeoutFailure);
+      assertEquals(
+          TimeoutType.TIMEOUT_TYPE_START_TO_CLOSE, ((TimeoutFailure) cause).getTimeoutType());
+    }
   }
 }

temporalio/sdk-java

temporal-testing/src/main/java/io/temporal/internal/testservice/SelfAdvancingTimerImpl.java
@@ -23,10 +23,7 @@ import com.google.common.util.concurrent.Uninterruptibles;
 import io.temporal.workflow.Functions;
 import java.sql.Timestamp;
 import java.time.Duration;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
@@ -97,7 +94,13 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
       while (!Thread.currentThread().isInterrupted()) {
         updateTimeLocked();
         if (!emptyQueue && tasks.isEmpty()) {
-          lockTimeSkippingLocked("runLocked"); // Switching to wall time when no tasks scheduled
+          if (timeLockOnEmptyQueueHandle != null) {
+            throw new IllegalStateException(
+                "SelfAdvancingTimerImpl should have no taken time lock when queue is not empty, but handle is not null");
+          }
+          // Switching to wall time when no tasks scheduled
+          timeLockOnEmptyQueueHandle =
+              lockTimeSkippingLocked("SelfAdvancingTimerImpl runLocked empty-queue");
           emptyQueue = true;
         }
         TimerTask peekedTask = tasks.peek();
@@ -181,6 +184,11 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
 
     @Override
     public void unlock() {
+      unlock(null);
+    }
+
+    @Override
+    public void unlock(String caller) {
       lock.lock();
       try {
         unlockFromHandleLocked();
@@ -191,7 +199,7 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
     }
 
     private void unlockFromHandleLocked() {
-      Boolean removed = lockEvents.remove(event);
+      boolean removed = lockEvents.remove(event);
       if (!removed) {
         throw new IllegalStateException("Unbalanced lock and unlock calls");
       }
@@ -225,12 +233,13 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
   private final PriorityQueue<TimerTask> tasks =
       new PriorityQueue<>(Comparator.comparing(TimerTask::getExecutionTime));
   private final Thread timerPump = new Thread(new TimerPump(), "SelfAdvancingTimer Pump");
+  private LockHandle timeLockOnEmptyQueueHandle;
 
   public SelfAdvancingTimerImpl(long initialTime) {
     currentTime = initialTime == 0 ? System.currentTimeMillis() : initialTime;
     executor.setRejectedExecutionHandler(new CallerRunsPolicy());
     // Queue is initially empty. The code assumes that in this case skipping is already locked.
-    lockTimeSkipping("SelfAdvancingTimerImpl constructor");
+    timeLockOnEmptyQueueHandle = lockTimeSkipping("SelfAdvancingTimerImpl constructor empty-queue");
     timerPump.start();
   }
 
@@ -277,7 +286,13 @@ final class SelfAdvancingTimerImpl implements SelfAdvancingTimer {
       tasks.add(timerTask);
       // Locked when queue became empty
       if (tasks.size() == 1 && emptyQueue) {
-        unlockTimeSkippingLocked("schedule task for " + taskInfo);
+        if (timeLockOnEmptyQueueHandle == null) {
+          throw new IllegalStateException(
+              "SelfAdvancingTimerImpl should take a lock and get a handle when queue is empty, but handle is null");
+        }
+        timeLockOnEmptyQueueHandle.unlock(
+            "SelfAdvancingTimerImpl schedule non-empty-queue, task: " + taskInfo);
+        timeLockOnEmptyQueueHandle = null;
         emptyQueue = false;
       }
       condition.signal();

temporal-testing/src/test/java/io/temporal/testing/TestWorkflowEnvironmentSleepTest.java
@@ -19,8 +19,14 @@
 
 package io.temporal.testing;
 
+import static org.junit.Assert.*;
+
+import io.temporal.api.enums.v1.TimeoutType;
 import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowFailedException;
 import io.temporal.client.WorkflowOptions;
+import io.temporal.client.WorkflowStub;
+import io.temporal.failure.TimeoutFailure;
 import io.temporal.worker.Worker;
 import io.temporal.workflow.SignalMethod;
 import io.temporal.workflow.Workflow;
@@ -57,7 +63,7 @@ public class TestWorkflowEnvironmentSleepTest {
     void signal();
   }
 
-  public static class ExampleWorkflowImpl implements ExampleWorkflow {
+  public static class HangingWorkflowWithSignalImpl implements ExampleWorkflow {
     @Override
     public void execute() {
       Workflow.sleep(Duration.ofMinutes(20));
@@ -70,7 +76,6 @@ public class TestWorkflowEnvironmentSleepTest {
   private TestWorkflowEnvironment testEnv;
   private Worker worker;
   private WorkflowClient client;
-  private ExampleWorkflow workflow;
   private static final String WORKFLOW_TASK_QUEUE = "EXAMPLE";
 
   @Before
@@ -78,23 +83,49 @@ public class TestWorkflowEnvironmentSleepTest {
     testEnv = TestWorkflowEnvironment.newInstance();
     worker = testEnv.newWorker(WORKFLOW_TASK_QUEUE);
     client = testEnv.getWorkflowClient();
-    worker.registerWorkflowImplementationTypes(ExampleWorkflowImpl.class);
-    workflow =
-        client.newWorkflowStub(
-            ExampleWorkflow.class,
-            WorkflowOptions.newBuilder().setTaskQueue(WORKFLOW_TASK_QUEUE).build());
+    worker.registerWorkflowImplementationTypes(HangingWorkflowWithSignalImpl.class);
     testEnv.start();
-  }
-
-  @Test(timeout = 2000)
-  public void testSignalAfterStartThenSleep() {
-    WorkflowClient.start(workflow::execute);
-    workflow.signal();
-    testEnv.sleep(Duration.ofMinutes(50L));
   }
 
   @After
   public void tearDown() {
     testEnv.close();
+  }
+
+  @Test(timeout = 2000)
+  public void testSignalAfterStartThenSleep() {
+    ExampleWorkflow workflow =
+        client.newWorkflowStub(
+            ExampleWorkflow.class,
+            WorkflowOptions.newBuilder().setTaskQueue(WORKFLOW_TASK_QUEUE).build());
+    WorkflowClient.start(workflow::execute);
+    workflow.signal();
+    testEnv.sleep(Duration.ofMinutes(50L));
+  }
+
+  @Test
+  public void testWorkflowTimeoutDuringSleep() {
+    ExampleWorkflow workflow =
+        client.newWorkflowStub(
+            ExampleWorkflow.class,
+            WorkflowOptions.newBuilder()
+                .setWorkflowExecutionTimeout(Duration.ofMinutes(3))
+                .setTaskQueue(WORKFLOW_TASK_QUEUE)
+                .build());
+
+    WorkflowClient.start(workflow::execute);
+
+    testEnv.sleep(Duration.ofMinutes(11L));
+
+    WorkflowStub workflowStub = WorkflowStub.fromTyped(workflow);
+    try {
+      workflowStub.getResult(Void.class);
+      fail("Workflow should fail with timeout exception");
+    } catch (WorkflowFailedException e) {
+      Throwable cause = e.getCause();
+      assertTrue(cause instanceof TimeoutFailure);
+      assertEquals(
+          TimeoutType.TIMEOUT_TYPE_START_TO_CLOSE, ((TimeoutFailure) cause).getTimeoutType());
+    }
   }
 }