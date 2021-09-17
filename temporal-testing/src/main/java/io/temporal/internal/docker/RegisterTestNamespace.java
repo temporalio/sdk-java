274b0ccf107bbb10c506cc838faf3395298ebd3f
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2021-09-17 01:39:04 -0400

    Implement a workaround for #1941
    Various cleanups of test resources

--- temporal-testing/src/main/java/io/temporal/internal/docker/RegisterTestNamespace.java
+++ temporal-testing/src/main/java/io/temporal/internal/docker/RegisterTestNamespace.java
@@ -22,6 +22,9 @@ package io.temporal.internal.docker;
 import com.google.protobuf.util.Durations;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import io.temporal.api.workflowservice.v1.DescribeNamespaceResponse;
+import io.temporal.api.workflowservice.v1.ListNamespacesRequest;
+import io.temporal.api.workflowservice.v1.ListNamespacesResponse;
 import io.temporal.api.workflowservice.v1.RegisterNamespaceRequest;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.serviceclient.WorkflowServiceStubsOptions;
@@ -38,9 +41,30 @@ public class RegisterTestNamespace {
       return;
     }
 
-    WorkflowServiceStubs service =
-        WorkflowServiceStubs.newInstance(
-            WorkflowServiceStubsOptions.newBuilder().setTarget(serviceAddress).build());
+    WorkflowServiceStubsOptions.Builder options = WorkflowServiceStubsOptions.newBuilder();
+    if (serviceAddress != null) {
+      options.setTarget(serviceAddress);
+    }
+
+    WorkflowServiceStubs service = null;
+    try {
+      service = WorkflowServiceStubs.newInstance(options.build());
+      if (doesNamespaceExist(service)) {
+        System.out.println("Namespace " + NAMESPACE + " already exists");
+      } else {
+        registerNamespace(service);
+        waitForNamespace(service);
+      }
+      System.exit(0);
+    } finally {
+      if (service != null) {
+        service.shutdown();
+      }
+    }
+    System.exit(0);
+  }
+
+  private static void registerNamespace(WorkflowServiceStubs service) throws InterruptedException {
     RegisterNamespaceRequest request =
         RegisterNamespaceRequest.newBuilder()
             .setNamespace(NAMESPACE)
@@ -49,9 +73,11 @@ public class RegisterTestNamespace {
     while (true) {
       try {
         service.blockingStub().registerNamespace(request);
+        System.out.println("Namespace " + NAMESPACE + " registered");
         break;
       } catch (StatusRuntimeException e) {
         if (e.getStatus().getCode() == Status.Code.ALREADY_EXISTS) {
+          System.out.println("Namespace " + NAMESPACE + " already exists");
           break;
         }
         if (e.getStatus().getCode() == Status.Code.DEADLINE_EXCEEDED
@@ -64,6 +90,27 @@ public class RegisterTestNamespace {
         System.exit(1);
       }
     }
-    System.exit(0);
+  }
+
+  private static void waitForNamespace(WorkflowServiceStubs service) throws InterruptedException {
+    // We have a namespace cache on the server side which is refreshed every 10seconds. All
+    // namespace centric APIs
+    // don't use the cache, so the best way is just to wait for it to expire. Another way is to call
+    // some
+    // workflow/activity tasks poll APIs and wait till they stop failing
+    // https://github.com/temporalio/temporal/issues/1941
+    System.out.println("Waiting for the namespace propagation...");
+    Thread.sleep(10000);
+  }
+
+  private static boolean doesNamespaceExist(WorkflowServiceStubs service) {
+    ListNamespacesRequest request = ListNamespacesRequest.newBuilder().build();
+    ListNamespacesResponse listNamespacesResponse = service.blockingStub().listNamespaces(request);
+    for (DescribeNamespaceResponse namespaceResponse : listNamespacesResponse.getNamespacesList()) {
+      if (namespaceResponse.getNamespaceInfo().getName().equals(NAMESPACE)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
