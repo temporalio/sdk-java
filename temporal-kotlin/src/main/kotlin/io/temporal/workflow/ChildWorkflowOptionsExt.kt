4b7c8e1ef75d4538740370bc302e2b9852e29f5d
Mykola Rybak <mykola.rybak@gmail.com>
2021-09-17 17:05:23 +0300

    Add extensions for workflow code

---
+++ temporal-kotlin/src/main/kotlin/io/temporal/workflow/ChildWorkflowOptionsExt.kt
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (C) 2020 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package io.temporal.workflow
+
+import io.temporal.common.RetryOptions
+import io.temporal.kotlin.TemporalDsl
+
+inline fun ChildWorkflowOptions(
+  options: @TemporalDsl ChildWorkflowOptions.Builder.() -> Unit
+): ChildWorkflowOptions {
+  return ChildWorkflowOptions.newBuilder().apply(options).build()
+}
+
+/**
+ * [RetryOptions] that define how child workflow is retried in case of failure. Default is no
+ * reties.
+ */
+inline fun ChildWorkflowOptions.Builder.setRetryOptions(
+  retryOptions: @TemporalDsl RetryOptions.Builder.() -> Unit
+) {
+  setRetryOptions(RetryOptions(retryOptions))
+}
