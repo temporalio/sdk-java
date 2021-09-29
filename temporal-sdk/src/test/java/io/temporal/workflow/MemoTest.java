040265ddc51415df2e2b4895b42c405669bf97a9
jeffschoner-stripe <63118764+jeffschoner-stripe@users.noreply.github.com>
2021-09-29 13:25:31 -0700

    Handle case where getMemo requests a non-existent key (#783)
    Co-authored-by: Dmitry Spikhalskiy <dmitry@temporal.io>

--- temporal-sdk/src/test/java/io/temporal/workflow/MemoTest.java
+++ temporal-sdk/src/test/java/io/temporal/workflow/MemoTest.java
@@ -19,8 +19,7 @@
 
 package io.temporal.workflow;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.reflect.TypeToken;
@@ -33,11 +32,11 @@ import io.temporal.api.history.v1.HistoryEvent;
 import io.temporal.api.workflowservice.v1.GetWorkflowExecutionHistoryResponse;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
+import io.temporal.common.converter.DataConverterException;
 import io.temporal.common.converter.GsonJsonPayloadConverter;
 import io.temporal.internal.client.WorkflowClientHelper;
 import io.temporal.testing.internal.SDKTestOptions;
 import io.temporal.testing.internal.SDKTestWorkflowRule;
-import io.temporal.workflow.shared.TestMultiArgWorkflowFunctions.TestMultiArgWorkflowImpl;
 import io.temporal.workflow.shared.TestMultiArgWorkflowFunctions.TestNoArgsWorkflowFunc;
 import io.temporal.workflow.shared.TestWorkflows.NoArgsWorkflow;
 import java.util.HashMap;
@@ -51,6 +50,7 @@ public class MemoTest {
   private static final String MEMO_VALUE = "testValue";
   private static final String MEMO_KEY_2 = "testKey2";
   private static final Integer MEMO_VALUE_2 = 1;
+  private static final String MEMO_KEY_MISSING = "testKey3";
   private static final Map<String, Object> MEMO =
       ImmutableMap.of(
           MEMO_KEY,
@@ -64,9 +64,11 @@ public class MemoTest {
 
   @Rule
   public SDKTestWorkflowRule testWorkflowRule =
-      SDKTestWorkflowRule.newBuilder()
-          .setWorkflowTypes(TestMultiArgWorkflowImpl.class, WorkflowImpl.class)
-          .build();
+      SDKTestWorkflowRule.newBuilder().setWorkflowTypes(WorkflowWithMemoImpl.class).build();
+
+  @Rule
+  public SDKTestWorkflowRule testNoMemoWorkflowRule =
+      SDKTestWorkflowRule.newBuilder().setWorkflowTypes(WorkflowWithoutMemoImpl.class).build();
 
   @Test
   public void testMemo() {
@@ -116,15 +118,54 @@ public class MemoTest {
     workflow.execute();
   }
 
-  public static class WorkflowImpl implements NoArgsWorkflow {
+  @Test
+  public void testNoMemoInWorkflowFailsGetMemoGracefully() {
+    WorkflowOptions workflowOptions =
+        SDKTestOptions.newWorkflowOptionsWithTimeouts(testNoMemoWorkflowRule.getTaskQueue())
+            .toBuilder()
+            .setTaskQueue(testNoMemoWorkflowRule.getTaskQueue())
+            .build();
+
+    NoArgsWorkflow workflow =
+        testNoMemoWorkflowRule
+            .getWorkflowClient()
+            .newWorkflowStub(NoArgsWorkflow.class, workflowOptions);
+    workflow.execute();
+  }
+
+  public static class WorkflowWithMemoImpl implements NoArgsWorkflow {
     @Override
     public void execute() {
+      // Simple value found
       assertEquals(MEMO_VALUE, Workflow.getMemo(MEMO_KEY, String.class));
+
+      // Map value found
       Map result =
           Workflow.getMemo(
               MEMO_KEY_2, Map.class, new TypeToken<HashMap<String, Integer>>() {}.getType());
       assertTrue(result instanceof HashMap);
       assertEquals(MEMO_VALUE_2, result.get(MEMO_KEY_2));
+
+      // Requested mismatched type
+      boolean throwsDataConverterException = false;
+      try {
+        Workflow.getMemo(MEMO_KEY, Integer.class);
+      } catch (DataConverterException e) {
+        throwsDataConverterException = true;
+      } finally {
+        assertTrue(throwsDataConverterException);
+      }
+
+      // Missing key not found
+      assertNull(Workflow.getMemo(MEMO_KEY_MISSING, String.class));
+    }
+  }
+
+  public static class WorkflowWithoutMemoImpl implements NoArgsWorkflow {
+    @Override
+    public void execute() {
+      // Missing key not found
+      assertNull(Workflow.getMemo(MEMO_KEY, String.class));
     }
   }
 }
