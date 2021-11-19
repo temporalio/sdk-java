0e12ca886393cde77cb7b8a31fb63d60d9a793f2
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2021-11-19 18:07:08 -0500

    Cleanup Activity exception javadocs Provide tests for behavior of activities in-flight when workflow execution is closing

--- temporal-sdk/src/main/java/io/temporal/client/ActivityCanceledException.java
+++ temporal-sdk/src/main/java/io/temporal/client/ActivityCanceledException.java
@@ -23,7 +23,17 @@ import io.temporal.activity.ActivityInfo;
 
 /**
  * Usually indicates that activity was already completed (duplicated request to complete) or timed
- * out or workflow is closed.
+ * out or cancellation was requested.
+ *
+ * <p>Catching this exception directly is discouraged and catching the parent class {@link
+ * ActivityCompletionException} is recommended instead.<br>
+ * If a workflow gets a cancellation request and it has activities started with {@link
+ * io.temporal.activity.ActivityCancellationType#TRY_CANCEL TRY_CANCEL}(default) or {@link
+ * io.temporal.activity.ActivityCancellationType#ABANDON ABANDON} cancellation type, the workflow
+ * may finish without waiting for activity cancellations and the activities will get {@link
+ * ActivityNotExistsException} from their heartbeat, not {@link ActivityCanceledException}. To
+ * handle the various edge cases, it's recommended to catch {@link ActivityCompletionException} and
+ * treat all the subclasses in the same way.
  */
 public final class ActivityCanceledException extends ActivityCompletionException {
 
