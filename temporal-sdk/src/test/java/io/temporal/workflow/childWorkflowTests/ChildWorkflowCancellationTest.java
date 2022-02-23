fc4836fc9e1362fb1967940259356d0125292a95
spikhalskiy <dmitry@spikhalskiy.com>
2022-02-23 14:59:40 -0500

    Fix flaky ChildWorkflowCancellationTest

--- temporal-sdk/src/test/java/io/temporal/workflow/childWorkflowTests/ChildWorkflowCancellationTest.java
+++ temporal-sdk/src/test/java/io/temporal/workflow/childWorkflowTests/ChildWorkflowCancellationTest.java
@@ -19,6 +19,7 @@
 
 package io.temporal.workflow.childWorkflowTests;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import io.temporal.api.common.v1.WorkflowExecution;
@@ -26,19 +27,25 @@ import io.temporal.api.enums.v1.EventType;
 import io.temporal.client.WorkflowFailedException;
 import io.temporal.client.WorkflowStub;
 import io.temporal.failure.CanceledFailure;
+import io.temporal.testUtils.Signal;
+import io.temporal.testing.internal.SDKTestOptions;
 import io.temporal.testing.internal.SDKTestWorkflowRule;
 import io.temporal.workflow.ChildWorkflowCancellationType;
 import io.temporal.workflow.ChildWorkflowOptions;
 import io.temporal.workflow.Workflow;
+import io.temporal.workflow.shared.TestActivities;
 import io.temporal.workflow.shared.TestActivities.TestActivitiesImpl;
 import io.temporal.workflow.shared.TestWorkflows.NoArgsWorkflow;
 import io.temporal.workflow.shared.TestWorkflows.TestWorkflowCancellationType;
 import java.time.Duration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class ChildWorkflowCancellationTest {
+
+  private static final Signal detachedScopeFinished = new Signal();
 
   @Rule
   public SDKTestWorkflowRule testWorkflowRule =
@@ -46,6 +53,11 @@ public class ChildWorkflowCancellationTest {
           .setWorkflowTypes(TestParentWorkflowImpl.class, TestChildWorkflowImpl.class)
           .setActivityImplementations(new TestActivitiesImpl())
           .build();
+
+  @Before
+  public void setUp() throws Exception {
+    detachedScopeFinished.clearSignal();
+  }
 
   @Test
   public void testChildWorkflowWaitCancellationRequested() {
@@ -65,6 +77,9 @@ public class ChildWorkflowCancellationTest {
         execution, EventType.EVENT_TYPE_EXTERNAL_WORKFLOW_EXECUTION_CANCEL_REQUESTED);
     testWorkflowRule.assertNoHistoryEvent(
         execution, EventType.EVENT_TYPE_CHILD_WORKFLOW_EXECUTION_CANCELED);
+    assertFalse(
+        "We should not have wait till the finish of detached scope in WAIT_CANCELLATION_REQUESTED mode",
+        detachedScopeFinished.isSignalled());
   }
 
   @Test
@@ -83,6 +98,9 @@ public class ChildWorkflowCancellationTest {
     }
     testWorkflowRule.assertHistoryEvent(
         execution, EventType.EVENT_TYPE_CHILD_WORKFLOW_EXECUTION_CANCELED);
+    assertTrue(
+        "We should be here after the full finish of the detached scope in WAIT_CANCELLATION_COMPLETED mode",
+        detachedScopeFinished.isSignalled());
   }
 
   @Test
@@ -100,6 +118,9 @@ public class ChildWorkflowCancellationTest {
     }
     testWorkflowRule.assertNoHistoryEvent(
         execution, EventType.EVENT_TYPE_REQUEST_CANCEL_EXTERNAL_WORKFLOW_EXECUTION_INITIATED);
+    assertFalse(
+        "We should not have wait till the finish of detached scope in ABANDON mode",
+        detachedScopeFinished.isSignalled());
   }
 
   @Test
@@ -119,6 +140,9 @@ public class ChildWorkflowCancellationTest {
         execution, EventType.EVENT_TYPE_REQUEST_CANCEL_EXTERNAL_WORKFLOW_EXECUTION_INITIATED);
     testWorkflowRule.assertNoHistoryEvent(
         execution, EventType.EVENT_TYPE_EXTERNAL_WORKFLOW_EXECUTION_CANCEL_REQUESTED);
+    assertFalse(
+        "We should not have wait till the finish of detached scope in TRY_CANCEL mode",
+        detachedScopeFinished.isSignalled());
   }
 
   public static class TestParentWorkflowImpl implements TestWorkflowCancellationType {
@@ -134,12 +158,21 @@ public class ChildWorkflowCancellationTest {
   }
 
   public static class TestChildWorkflowImpl implements NoArgsWorkflow {
+
     @Override
     public void execute() {
+      TestActivities.VariousTestActivities localActivities =
+          Workflow.newLocalActivityStub(
+              TestActivities.VariousTestActivities.class, SDKTestOptions.newLocalActivityOptions());
       try {
         Workflow.sleep(Duration.ofHours(1));
       } catch (CanceledFailure e) {
-        Workflow.newDetachedCancellationScope(() -> Workflow.sleep(Duration.ofSeconds(1))).run();
+        Workflow.newDetachedCancellationScope(
+                () -> {
+                  localActivities.sleepActivity(5000, 0);
+                  detachedScopeFinished.signal();
+                })
+            .run();
       }
     }
   }
