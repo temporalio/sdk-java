e943114e77c73d3d296e8f59eb48a5ebc97d602c
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2022-03-23 14:15:45 -0400

    Improve client WorkflowFailedException message with the reason
    Improves top client WorkflowFailedException message by making it more human-readable and enriches it with the reason (terminated, canceled, timed_out, failed) to make what happened with the workflow immediately clear without looking at the root exception down the stack.
    Simplifies exception wrapping and handling to avoid non-needed intermediate representation.

--- temporal-sdk/src/main/java/io/temporal/client/WorkflowFailedException.java
+++ temporal-sdk/src/main/java/io/temporal/client/WorkflowFailedException.java
@@ -20,6 +20,7 @@
 package io.temporal.client;
 
 import io.temporal.api.common.v1.WorkflowExecution;
+import io.temporal.api.enums.v1.EventType;
 import io.temporal.api.enums.v1.RetryState;
 
 /**
@@ -28,20 +29,28 @@ import io.temporal.api.enums.v1.RetryState;
  */
 public final class WorkflowFailedException extends WorkflowException {
 
+  private final EventType workflowCloseEventType;
   private final RetryState retryState;
   private final long workflowTaskCompletedEventId;
 
   public WorkflowFailedException(
       WorkflowExecution workflowExecution,
       String workflowType,
+      EventType workflowCloseEventType,
       long workflowTaskCompletedEventId,
       RetryState retryState,
       Throwable cause) {
     super(
-        getMessage(workflowExecution, workflowType, workflowTaskCompletedEventId, retryState),
+        getMessage(
+            workflowExecution,
+            workflowType,
+            workflowCloseEventType,
+            workflowTaskCompletedEventId,
+            retryState),
         workflowExecution,
         workflowType,
         cause);
+    this.workflowCloseEventType = workflowCloseEventType;
     this.retryState = retryState;
     this.workflowTaskCompletedEventId = workflowTaskCompletedEventId;
   }
@@ -50,23 +59,72 @@ public final class WorkflowFailedException extends WorkflowException {
     return retryState;
   }
 
+  /**
+   * This value is defined only if workflow failure is caused by an explicit WORKFLOW_TASK_COMPLETED
+   * command. If workflow timed out, was cancelled or terminated, this value is undefined.
+   *
+   * @return eventId of the WORKFLOW_TASK_COMPLETED event that reported (caused)
+   *     WORKFLOW_EXECUTION_FAILED command. -1 if undefined.
+   */
   public long getWorkflowTaskCompletedEventId() {
     return workflowTaskCompletedEventId;
   }
 
-  public static String getMessage(
+  /**
+   * Returns Event Type that caused {@code this} exception. This Event Type should be one of
+   *
+   * <ul>
+   *   <li>{@link EventType#EVENT_TYPE_WORKFLOW_EXECUTION_CANCELED}
+   *   <li>{@link EventType#EVENT_TYPE_WORKFLOW_EXECUTION_TERMINATED}
+   *   <li>{@link EventType#EVENT_TYPE_WORKFLOW_EXECUTION_TIMED_OUT}
+   *   <li>{@link EventType#EVENT_TYPE_WORKFLOW_EXECUTION_FAILED}
+   * </ul>
+   *
+   * @return event type that caused {@code this} exception
+   */
+  public EventType getWorkflowCloseEventType() {
+    return workflowCloseEventType;
+  }
+
+  private static String getMessage(
       WorkflowExecution workflowExecution,
       String workflowType,
+      EventType closeEventType,
       long workflowTaskCompletedEventId,
       RetryState retryState) {
-    return "workflowId='"
+    return "Workflow execution "
+        + "{"
+        + "workflowId='"
         + workflowExecution.getWorkflowId()
         + "', runId='"
         + workflowExecution.getRunId()
-        + (workflowType == null ? "'" : "', workflowType='" + workflowType + '\'')
-        + ", retryState="
+        + (workflowType == null ? "'" : "', workflowType='" + workflowType + "'")
+        + "} "
+        + getAction(closeEventType)
+        + " with "
+        + "{"
+        + "closeEventType='"
+        + closeEventType
+        + "', retryState='"
         + retryState
-        + ", workflowTaskCompletedEventId="
-        + workflowTaskCompletedEventId;
+        + (workflowTaskCompletedEventId == -1
+            ? "'"
+            : "', workflowTaskCompletedEventId=" + workflowTaskCompletedEventId + "'")
+        + "}";
+  }
+
+  private static String getAction(EventType closeEventType) {
+    switch (closeEventType) {
+      case EVENT_TYPE_WORKFLOW_EXECUTION_CANCELED:
+        return "was cancelled";
+      case EVENT_TYPE_WORKFLOW_EXECUTION_TERMINATED:
+        return "was terminated";
+      case EVENT_TYPE_WORKFLOW_EXECUTION_TIMED_OUT:
+        return "timed out";
+      case EVENT_TYPE_WORKFLOW_EXECUTION_FAILED:
+        return "failed";
+      default:
+        return "failed with an unexpected closing event type " + closeEventType;
+    }
   }
 }
