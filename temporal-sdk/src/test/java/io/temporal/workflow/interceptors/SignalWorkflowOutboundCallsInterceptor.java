82e235b519a2a277263e2f5c3f87311e06f6e78d
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2022-04-09 21:44:04 -0400

    Tests verifying that getVersion inside a signal handler works correctly

--- temporal-sdk/src/test/java/io/temporal/workflow/interceptors/SignalWorkflowOutboundCallsInterceptor.java
+++ temporal-sdk/src/test/java/io/temporal/workflow/interceptors/SignalWorkflowOutboundCallsInterceptor.java
@@ -20,51 +20,20 @@
 package io.temporal.workflow.interceptors;
 
 import io.temporal.common.interceptors.WorkflowOutboundCallsInterceptor;
-import io.temporal.workflow.Functions;
-import io.temporal.workflow.Promise;
-import java.lang.reflect.Type;
-import java.time.Duration;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Random;
-import java.util.UUID;
-import java.util.function.BiPredicate;
+import io.temporal.common.interceptors.WorkflowOutboundCallsInterceptorBase;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
-public class SignalWorkflowOutboundCallsInterceptor implements WorkflowOutboundCallsInterceptor {
-
+public class SignalWorkflowOutboundCallsInterceptor extends WorkflowOutboundCallsInterceptorBase {
   private final Function<Object[], Object[]> overrideArgs;
   private final Function<String, String> overrideSignalName;
-  private final WorkflowOutboundCallsInterceptor next;
 
   public SignalWorkflowOutboundCallsInterceptor(
       Function<Object[], Object[]> overrideArgs,
       Function<String, String> overrideSignalName,
       WorkflowOutboundCallsInterceptor next) {
+    super(next);
     this.overrideArgs = overrideArgs;
     this.overrideSignalName = overrideSignalName;
-    this.next = Objects.requireNonNull(next);
-  }
-
-  @Override
-  public <T> ActivityOutput<T> executeActivity(ActivityInput<T> input) {
-    return next.executeActivity(input);
-  }
-
-  @Override
-  public <R> LocalActivityOutput<R> executeLocalActivity(LocalActivityInput<R> input) {
-    return next.executeLocalActivity(input);
-  }
-
-  @Override
-  public <R> ChildWorkflowOutput<R> executeChildWorkflow(ChildWorkflowInput<R> input) {
-    return next.executeChildWorkflow(input);
-  }
-
-  @Override
-  public Random newRandom() {
-    return next.newRandom();
   }
 
   @Override
@@ -73,100 +42,10 @@ public class SignalWorkflowOutboundCallsInterceptor implements WorkflowOutboundC
     if (args != null && args.length > 0) {
       args = new Object[] {"corrupted signal"};
     }
-    return next.signalExternalWorkflow(
+    return super.signalExternalWorkflow(
         new SignalExternalInput(
             input.getExecution(),
             overrideSignalName.apply(input.getSignalName()),
             overrideArgs.apply(args)));
-  }
-
-  @Override
-  public CancelWorkflowOutput cancelWorkflow(CancelWorkflowInput input) {
-    return next.cancelWorkflow(input);
-  }
-
-  @Override
-  public void sleep(Duration duration) {
-    next.sleep(duration);
-  }
-
-  @Override
-  public boolean await(Duration timeout, String reason, Supplier<Boolean> unblockCondition) {
-    return next.await(timeout, reason, unblockCondition);
-  }
-
-  @Override
-  public void await(String reason, Supplier<Boolean> unblockCondition) {
-    next.await(reason, unblockCondition);
-  }
-
-  @Override
-  public Promise<Void> newTimer(Duration duration) {
-    return next.newTimer(duration);
-  }
-
-  @Override
-  public <R> R sideEffect(Class<R> resultClass, Type resultType, Functions.Func<R> func) {
-    return next.sideEffect(resultClass, resultType, func);
-  }
-
-  @Override
-  public <R> R mutableSideEffect(
-      String id,
-      Class<R> resultClass,
-      Type resultType,
-      BiPredicate<R, R> updated,
-      Functions.Func<R> func) {
-    return null;
-  }
-
-  @Override
-  public int getVersion(String changeId, int minSupported, int maxSupported) {
-    return next.getVersion(changeId, minSupported, maxSupported);
-  }
-
-  @Override
-  public void continueAsNew(ContinueAsNewInput input) {
-    next.continueAsNew(input);
-  }
-
-  @Override
-  public void registerQuery(RegisterQueryInput input) {
-    next.registerQuery(input);
-  }
-
-  @Override
-  public void registerSignalHandlers(RegisterSignalHandlersInput input) {
-    next.registerSignalHandlers(input);
-  }
-
-  @Override
-  public void registerDynamicSignalHandler(RegisterDynamicSignalHandlerInput input) {
-    next.registerDynamicSignalHandler(input);
-  }
-
-  @Override
-  public void registerDynamicQueryHandler(RegisterDynamicQueryHandlerInput input) {
-    next.registerDynamicQueryHandler(input);
-  }
-
-  @Override
-  public UUID randomUUID() {
-    return next.randomUUID();
-  }
-
-  @Override
-  public void upsertSearchAttributes(Map<String, Object> searchAttributes) {
-    next.upsertSearchAttributes(searchAttributes);
-  }
-
-  @Override
-  public Object newChildThread(Runnable runnable, boolean detached, String name) {
-    return next.newChildThread(runnable, detached, name);
-  }
-
-  @Override
-  public long currentTimeMillis() {
-    return next.currentTimeMillis();
   }
 }
