temporalio/sdk-java

commit b079eb2796df4e775f54bd369673d63b04d4549f

Open WorkflowExecutionHistory as a public class (#1566)

temporal-sdk/src/main/java/io/temporal/common/WorkflowExecutionHistory.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright (C) 2022 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ * Copyright (C) 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this material except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.temporal.common;
+
+import io.temporal.api.common.v1.WorkflowExecution;
+import io.temporal.api.history.v1.History;
+import io.temporal.api.history.v1.HistoryEvent;
+import java.util.List;
+
+/**
+ * Provides a wrapper with convenience methods over raw protobuf {@link History} object representing
+ * workflow history
+ */
+@SuppressWarnings("deprecation")
+public final class WorkflowExecutionHistory
+    extends io.temporal.internal.common.WorkflowExecutionHistory {
+
+  /**
+   * @param history raw history object to enrich
+   */
+  public WorkflowExecutionHistory(History history) {
+    super(history);
+  }
+
+  /**
+   * WorkflowId is not persisted in workflow history, and sometimes it may be important to have it
+   * set (workflow replay may rely on it if WorkflowExecutionHistory is used for a history replay)
+   *
+   * @param history raw history object to enrich
+   * @param workflowId workflow id to use in {@link #getWorkflowExecution()}
+   */
+  public WorkflowExecutionHistory(History history, String workflowId) {
+    super(history, workflowId);
+  }
+
+  /**
+   * @param serialized history json (tctl format) to import and deserialize into {@link History}
+   * @return WorkflowExecutionHistory
+   */
+  @SuppressWarnings("deprecation")
+  public static WorkflowExecutionHistory fromJson(String serialized) {
+    return io.temporal.internal.common.WorkflowExecutionHistory.fromJson(serialized);
+  }
+
+  /**
+   * @return full json that can be used for replay and which is compatible with tctl
+   */
+  public String toJson(boolean prettyPrint) {
+    return super.toJson(prettyPrint);
+  }
+
+  /**
+   * Returns workflow instance history in a human readable format.
+   *
+   * @param showWorkflowTasks when set to false workflow task events (command events) are not
+   *     included
+   */
+  public String toProtoText(boolean showWorkflowTasks) {
+    return super.toProtoText(showWorkflowTasks);
+  }
+
+  @Override
+  public WorkflowExecution getWorkflowExecution() {
+    return super.getWorkflowExecution();
+  }
+
+  public List<HistoryEvent> getEvents() {
+    return super.getEvents();
+  }
+
+  public HistoryEvent getLastEvent() {
+    return super.getLastEvent();
+  }
+
+  public History getHistory() {
+    return super.getHistory();
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+}

temporal-sdk/src/main/java/io/temporal/internal/common/WorkflowExecutionHistory.java
@@ -33,11 +33,16 @@ import io.temporal.api.history.v1.HistoryEvent;
 import io.temporal.common.converter.DataConverterException;
 import java.util.List;
 
+// This class by mistake leaked into a public interface and is used by users, so it can't be deleted
+// right away.
+// To fix the mistake, it was republished in a public package as
+// io.temporal.common.WorkflowExecutionHistory class
+// that extends this class.
 /**
- * Provides a wrapper with convenience methods over raw protobuf {@link History} object representing
- * workflow history
+ * @deprecated use {@link io.temporal.common.WorkflowExecutionHistory} instead.
  */
-public final class WorkflowExecutionHistory {
+@Deprecated
+public class WorkflowExecutionHistory {
   private static final Gson GSON_PRETTY_PRINTER = new GsonBuilder().setPrettyPrinting().create();
   // we stay on using the old API that uses a JsonParser instance instead of static methods
   // to give users a larger range of supported version
@@ -57,7 +62,7 @@ public final class WorkflowExecutionHistory {
     this.workflowId = workflowId;
   }
 
-  public static WorkflowExecutionHistory fromJson(String serialized) {
+  public static io.temporal.common.WorkflowExecutionHistory fromJson(String serialized) {
     String protoJson = HistoryJsonUtils.historyFormatJsonToProtoJson(serialized);
 
     JsonFormat.Parser parser = JsonFormat.parser().ignoringUnknownFields();
@@ -68,7 +73,7 @@ public final class WorkflowExecutionHistory {
       throw new DataConverterException(e);
     }
     History history = historyBuilder.build();
-    return new WorkflowExecutionHistory(history);
+    return new io.temporal.common.WorkflowExecutionHistory(history);
   }
 
   private static void checkHistory(History history) {

temporal-sdk/src/main/java/io/temporal/internal/common/HistoryJsonUtils.java
@@ -31,7 +31,7 @@ import java.util.function.BiFunction;
  * supported by {@link com.google.protobuf.util.JsonFormat} to the format of Temporal history
  * supported by tctl and back.
  */
-class HistoryJsonUtils {
+public final class HistoryJsonUtils {
   private static final Configuration JSON_PATH_CONFIGURATION =
       Configuration.builder().options(Option.SUPPRESS_EXCEPTIONS).build();
 
@@ -57,6 +57,8 @@ class HistoryJsonUtils {
       this.protobufEnumPrefix = protobufEnumPrefix;
     }
   }
+
+  private HistoryJsonUtils() {}
 
   public static String protoJsonToHistoryFormatJson(String protoJson) {
     return convertEnumValues(protoJson, ProtoEnumNameUtils::uniqueToSimplifiedName);

temporal-sdk/src/main/java/io/temporal/internal/common/HistoryProtoTextUtils.java
@@ -26,7 +26,7 @@ import io.temporal.api.history.v1.History;
 import io.temporal.api.history.v1.HistoryEvent;
 
 /** Converts history protos into human readable format */
-final class HistoryProtoTextUtils {
+public final class HistoryProtoTextUtils {
 
   private HistoryProtoTextUtils() {}
 

temporal-sdk/src/main/java/io/temporal/internal/worker/QueryReplayHelper.java
@@ -29,7 +29,7 @@ import io.temporal.api.history.v1.WorkflowExecutionStartedEventAttributes;
 import io.temporal.api.query.v1.WorkflowQuery;
 import io.temporal.api.workflowservice.v1.PollWorkflowTaskQueueResponse;
 import io.temporal.api.workflowservice.v1.RespondQueryTaskCompletedRequest;
-import io.temporal.internal.common.WorkflowExecutionHistory;
+import io.temporal.common.WorkflowExecutionHistory;
 import java.util.List;
 import java.util.Optional;
 
@@ -57,16 +57,20 @@ public class QueryReplayHelper {
     return queryWorkflowExecution(queryType, args, history, ByteString.EMPTY);
   }
 
+  @SuppressWarnings("deprecation")
   public Optional<Payloads> queryWorkflowExecution(
-      WorkflowExecutionHistory history, String queryType, Optional<Payloads> args)
+      io.temporal.internal.common.WorkflowExecutionHistory history,
+      String queryType,
+      Optional<Payloads> args)
       throws Exception {
     return queryWorkflowExecution(queryType, args, history, ByteString.EMPTY);
   }
 
+  @SuppressWarnings("deprecation")
   private Optional<Payloads> queryWorkflowExecution(
       String queryType,
       Optional<Payloads> args,
-      WorkflowExecutionHistory history,
+      io.temporal.internal.common.WorkflowExecutionHistory history,
       ByteString nextPageToken)
       throws Exception {
     WorkflowQuery.Builder query = WorkflowQuery.newBuilder().setQueryType(queryType);

temporal-sdk/src/main/java/io/temporal/internal/worker/SyncWorkflowWorker.java
@@ -25,7 +25,6 @@ import io.temporal.common.converter.DataConverter;
 import io.temporal.internal.activity.ActivityExecutionContextFactory;
 import io.temporal.internal.activity.ActivityTaskHandlerImpl;
 import io.temporal.internal.activity.LocalActivityExecutionContextFactoryImpl;
-import io.temporal.internal.common.WorkflowExecutionHistory;
 import io.temporal.internal.replay.ReplayWorkflowTaskHandler;
 import io.temporal.internal.sync.POJOWorkflowImplementationFactory;
 import io.temporal.internal.sync.WorkflowThreadExecutor;
@@ -211,8 +210,9 @@ public class SyncWorkflowWorker implements SuspendableWorker {
     return workflowWorker.isSuspended();
   }
 
+  @SuppressWarnings("deprecation")
   public <R> R queryWorkflowExecution(
-      WorkflowExecutionHistory history,
+      io.temporal.internal.common.WorkflowExecutionHistory history,
       String queryType,
       Class<R> resultClass,
       Type resultType,

temporal-sdk/src/main/java/io/temporal/worker/Worker.java
@@ -27,10 +27,10 @@ import com.uber.m3.tally.Scope;
 import com.uber.m3.util.ImmutableMap;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowClientOptions;
+import io.temporal.common.WorkflowExecutionHistory;
 import io.temporal.common.context.ContextPropagator;
 import io.temporal.common.converter.DataConverter;
 import io.temporal.failure.TemporalFailure;
-import io.temporal.internal.common.WorkflowExecutionHistory;
 import io.temporal.internal.sync.WorkflowInternal;
 import io.temporal.internal.sync.WorkflowThreadExecutor;
 import io.temporal.internal.worker.*;
@@ -424,7 +424,9 @@ public final class Worker {
    * @throws Exception if replay failed for any reason
    */
   @VisibleForTesting
-  public void replayWorkflowExecution(WorkflowExecutionHistory history) throws Exception {
+  @SuppressWarnings("deprecation")
+  public void replayWorkflowExecution(io.temporal.internal.common.WorkflowExecutionHistory history)
+      throws Exception {
     workflowWorker.queryWorkflowExecution(
         history,
         WorkflowClient.QUERY_TYPE_REPLAY_ONLY,

temporal-sdk/src/test/java/io/temporal/internal/common/WorkflowExecutionHistoryTest.java
@@ -24,6 +24,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertEquals;
 
 import com.google.common.io.CharStreams;
+import io.temporal.common.WorkflowExecutionHistory;
 import io.temporal.testing.WorkflowHistoryLoader;
 import java.io.File;
 import java.io.IOException;

commit c2e0723233eb58d3b684278623133e9f708a9228

Fix binary compatibility broken by #1566

build.gradle
@@ -34,7 +34,7 @@ ext {
     micrometerVersion = '1.9.6' // [1.0.0,) // we don't upgrade to 1.10.x because it requires kotlin 1.6. Users may use 1.10.x in their environments though.
 
     slf4jVersion = '1.7.36' // [1.4.0,) // stay on 1.x for a while to don't use any APIs from 2.x which may break our users which stay on 1.x
-    protoVersion = '3.21.10' // [3.10.0,)
+    protoVersion = '3.21.11' // [3.10.0,)
     annotationApiVersion = '1.3.2'
     guavaVersion = '31.1-jre' // [10.0,)
     tallyVersion = '0.11.1' // [0.4.0,)

temporal-remote-data-encoder/build.gradle
@@ -20,7 +20,7 @@ dependencies {
 
     // Jetty 10+ brings a non-production ready slf4j that doesn't work with released logback.
     // It also require Java 11+. That's why we stay on Jetty 9. It's for tests only anyway.
-    testImplementation(platform("org.eclipse.jetty:jetty-bom:9.4.49.v20220914"))
+    testImplementation(platform("org.eclipse.jetty:jetty-bom:9.4.50.v20221201"))
     testImplementation ("org.eclipse.jetty:jetty-server")
     testImplementation ("org.eclipse.jetty:jetty-servlet")
 }

temporal-sdk/src/main/java/io/temporal/common/WorkflowExecutionHistory.java
@@ -20,9 +20,13 @@
 
 package io.temporal.common;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 import io.temporal.api.common.v1.WorkflowExecution;
 import io.temporal.api.history.v1.History;
 import io.temporal.api.history.v1.HistoryEvent;
+import io.temporal.common.converter.DataConverterException;
+import io.temporal.internal.common.HistoryJsonUtils;
 import java.util.List;
 
 /**
@@ -55,9 +59,18 @@ public final class WorkflowExecutionHistory
    * @param serialized history json (tctl format) to import and deserialize into {@link History}
    * @return WorkflowExecutionHistory
    */
-  @SuppressWarnings("deprecation")
   public static WorkflowExecutionHistory fromJson(String serialized) {
-    return io.temporal.internal.common.WorkflowExecutionHistory.fromJson(serialized);
+    String protoJson = HistoryJsonUtils.historyFormatJsonToProtoJson(serialized);
+
+    JsonFormat.Parser parser = JsonFormat.parser().ignoringUnknownFields();
+    History.Builder historyBuilder = History.newBuilder();
+    try {
+      parser.merge(protoJson, historyBuilder);
+    } catch (InvalidProtocolBufferException e) {
+      throw new DataConverterException(e);
+    }
+    History history = historyBuilder.build();
+    return new WorkflowExecutionHistory(history);
   }
 
   /**

temporal-sdk/src/main/java/io/temporal/internal/common/WorkflowExecutionHistory.java
@@ -62,18 +62,9 @@ public class WorkflowExecutionHistory {
     this.workflowId = workflowId;
   }
 
-  public static io.temporal.common.WorkflowExecutionHistory fromJson(String serialized) {
-    String protoJson = HistoryJsonUtils.historyFormatJsonToProtoJson(serialized);
-
-    JsonFormat.Parser parser = JsonFormat.parser().ignoringUnknownFields();
-    History.Builder historyBuilder = History.newBuilder();
-    try {
-      parser.merge(protoJson, historyBuilder);
-    } catch (InvalidProtocolBufferException e) {
-      throw new DataConverterException(e);
-    }
-    History history = historyBuilder.build();
-    return new io.temporal.common.WorkflowExecutionHistory(history);
+  public static WorkflowExecutionHistory fromJson(String serialized) {
+    return new WorkflowExecutionHistory(
+        io.temporal.common.WorkflowExecutionHistory.fromJson(serialized).getHistory());
   }
 
   private static void checkHistory(History history) {

temporal-serviceclient/build.gradle
@@ -57,7 +57,7 @@ protobuf {
     // For aarch64 (M1) using oldest version with binary available for this platform
     if (System.getProperty("os.arch") == 'aarch64') {
         protoc {
-            artifact = 'com.google.protobuf:protoc:3.21.10'
+            artifact = 'com.google.protobuf:protoc:3.21.11'
         }
         plugins {
             grpc {

temporal-spring-boot-autoconfigure-alpha/build.gradle
@@ -1,7 +1,7 @@
 description = '''Spring Boot AutoConfigure for Temporal Java SDK'''
 
 ext {
-    otelVersion = '1.20.1'
+    otelVersion = '1.21.0'
     otShimVersion = "${otelVersion}-alpha"
 }
 

temporal-test-server/build.gradle
@@ -61,7 +61,7 @@ protobuf {
     // For aarch64 (M1) using oldest version with binary available for this platform
     if (System.getProperty("os.arch") == 'aarch64') {
         protoc {
-            artifact = 'com.google.protobuf:protoc:3.21.10'
+            artifact = 'com.google.protobuf:protoc:3.21.11'
         }
         plugins {
             grpc {