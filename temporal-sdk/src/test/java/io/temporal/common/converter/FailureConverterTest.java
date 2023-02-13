1649f60d513924fb724be952cd97d31770d2583c
James Watkins-Harvey <mjameswh@gmail.com>
2023-02-13 18:16:13 -0500

    Add more tests

--- temporal-sdk/src/test/java/io/temporal/common/converter/FailureConverterTest.java
+++ temporal-sdk/src/test/java/io/temporal/common/converter/FailureConverterTest.java
@@ -26,36 +26,45 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import com.google.protobuf.ByteString;
+import io.temporal.activity.Activity;
+import io.temporal.activity.ActivityOptions;
 import io.temporal.api.common.v1.Payload;
 import io.temporal.api.failure.v1.Failure;
+import io.temporal.api.history.v1.HistoryEvent;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowClientOptions;
 import io.temporal.client.WorkflowException;
 import io.temporal.client.WorkflowOptions;
+import io.temporal.failure.ActivityFailure;
 import io.temporal.failure.ApplicationFailure;
 import io.temporal.failure.TemporalFailure;
+import io.temporal.internal.testing.WorkflowTestingTest;
 import io.temporal.internal.testing.WorkflowTestingTest.FailingWorkflowImpl;
 import io.temporal.payload.codec.PayloadCodec;
 import io.temporal.payload.codec.PayloadCodecException;
 import io.temporal.testing.TestEnvironmentOptions;
 import io.temporal.testing.TestWorkflowEnvironment;
 import io.temporal.worker.Worker;
+import io.temporal.workflow.Workflow;
+import io.temporal.workflow.shared.TestActivities;
 import io.temporal.workflow.shared.TestWorkflows.TestWorkflow1;
+import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.Timeout;
 
 public class FailureConverterTest {
   private static final String TASK_QUEUE = "test-workflow";
 
-  public @Rule Timeout timeout = Timeout.seconds(10);
+  //  public @Rule Timeout timeout = Timeout.seconds(10);
 
   private CodecDataConverter dataConverter;
+
+  private TestWorkflowEnvironment testEnvironment;
 
   @Before
   public void setUp() {
@@ -65,6 +74,19 @@ public class FailureConverterTest {
             DefaultDataConverter.newDefaultInstance(),
             Collections.singletonList(prefixPayloadCodec),
             true);
+
+    WorkflowClientOptions workflowClientOptions =
+        WorkflowClientOptions.newBuilder()
+            .setDataConverter(dataConverter)
+            .validateAndBuildWithDefaults();
+    TestEnvironmentOptions testEnvOptions =
+        TestEnvironmentOptions.newBuilder().setWorkflowClientOptions(workflowClientOptions).build();
+    testEnvironment = TestWorkflowEnvironment.newInstance(testEnvOptions);
+  }
+
+  @After
+  public void tearDown() {
+    testEnvironment.close();
   }
 
   @Test
@@ -144,35 +166,116 @@ public class FailureConverterTest {
   }
 
   @Test
-  public void testDefaultFailureAttributesEncodingEndToEnd() {
-    WorkflowClientOptions workflowClientOptions =
-        WorkflowClientOptions.newBuilder()
-            .setDataConverter(dataConverter)
-            .validateAndBuildWithDefaults();
-    TestEnvironmentOptions testEnvOptions =
-        TestEnvironmentOptions.newBuilder().setWorkflowClientOptions(workflowClientOptions).build();
+  public void testExceptionThrownFromWorkflowIsCorrectlyEncoded() {
+    Worker worker = testEnvironment.newWorker(TASK_QUEUE);
+    worker.registerWorkflowImplementationTypes(FailingWorkflowImpl.class);
+    testEnvironment.start();
 
-    try (TestWorkflowEnvironment testEnvironment =
-        TestWorkflowEnvironment.newInstance(testEnvOptions)) {
+    WorkflowClient client = testEnvironment.getWorkflowClient();
+    WorkflowOptions workflowOptions = WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).build();
+    TestWorkflow1 workflow = client.newWorkflowStub(TestWorkflow1.class, workflowOptions);
 
-      Worker worker = testEnvironment.newWorker(TASK_QUEUE);
-      worker.registerWorkflowImplementationTypes(FailingWorkflowImpl.class);
-      testEnvironment.start();
+    try {
+      workflow.execute("input1");
+      fail("unreacheable");
+    } catch (WorkflowException e) {
+      // Assert the exception can be correctly decoded
+      assertEquals(
+          "message='TestWorkflow1-input1', type='test', nonRetryable=false",
+          e.getCause().getMessage());
 
-      WorkflowClient client = testEnvironment.getWorkflowClient();
-      WorkflowOptions workflowOptions =
-          WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).build();
-      TestWorkflow1 workflow = client.newWorkflowStub(TestWorkflow1.class, workflowOptions);
+      List<HistoryEvent> history =
+          client
+              .fetchHistory(e.getExecution().getWorkflowId(), e.getExecution().getRunId())
+              .getHistory()
+              .getEventsList();
 
-      try {
-        workflow.execute("input1");
-        fail("unreacheable");
-      } catch (WorkflowException e) {
-        assertEquals(
-            "message='TestWorkflow1-input1', type='test', nonRetryable=false",
-            e.getCause().getMessage());
-      }
+      // Assert that exception is indeed encoded in the WorkflowExecutionFailedEvent
+      HistoryEvent wfeFailedEvent =
+          history.stream()
+              .filter((x) -> x.hasWorkflowExecutionFailedEventAttributes())
+              .findFirst()
+              .get();
+      assertTrue(
+          isEncoded(
+              wfeFailedEvent
+                  .getWorkflowExecutionFailedEventAttributes()
+                  .getFailure()
+                  .getEncodedAttributes()));
     }
+  }
+
+  @Test
+  public void testExceptionThrownFromActivityIsCorrectlyEncoded() {
+    Worker worker = testEnvironment.newWorker(TASK_QUEUE);
+    worker.registerWorkflowImplementationTypes(WorkflowTestingTest.ActivityWorkflow.class);
+    worker.registerActivitiesImplementations(new FailingActivityImpl());
+    testEnvironment.start();
+
+    WorkflowClient client = testEnvironment.getWorkflowClient();
+    WorkflowOptions workflowOptions = WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).build();
+    TestWorkflow1 workflow = client.newWorkflowStub(TestWorkflow1.class, workflowOptions);
+
+    try {
+      workflow.execute("input1");
+      fail("unreacheable");
+    } catch (WorkflowException e) {
+      // Assert the exception can be correctly decoded
+      assertEquals(
+          "Execute-input1", ((TemporalFailure) e.getCause().getCause()).getOriginalMessage());
+
+      List<HistoryEvent> history =
+          client
+              .fetchHistory(e.getExecution().getWorkflowId(), e.getExecution().getRunId())
+              .getHistory()
+              .getEventsList();
+
+      // Assert that exception is indeed encoded in the ActivityTaskFailedEvent
+      HistoryEvent actTaskFailedEvent =
+          history.stream()
+              .filter((x) -> x.hasActivityTaskFailedEventAttributes())
+              .findFirst()
+              .get();
+      assertTrue(
+          isEncoded(
+              actTaskFailedEvent
+                  .getActivityTaskFailedEventAttributes()
+                  .getFailure()
+                  .getEncodedAttributes()));
+
+      // Assert that activity's exception is still encoded in the WorkflowExecutionFailedEvent
+      HistoryEvent wfeFailedEvent =
+          history.stream()
+              .filter((x) -> x.hasWorkflowExecutionFailedEventAttributes())
+              .findFirst()
+              .get();
+      assertTrue(
+          isEncoded(
+              wfeFailedEvent
+                  .getWorkflowExecutionFailedEventAttributes()
+                  .getFailure()
+                  .getCause()
+                  .getEncodedAttributes()));
+    }
+  }
+
+  @Test
+  public void testExceptionThrownFromActivityIsReadableInWorkflow() {
+    Worker worker = testEnvironment.newWorker(TASK_QUEUE);
+    worker.registerWorkflowImplementationTypes(FailingActivityWorkflow.class);
+    worker.registerActivitiesImplementations(new FailingActivityImpl());
+    testEnvironment.start();
+
+    WorkflowClient client = testEnvironment.getWorkflowClient();
+    WorkflowOptions workflowOptions = WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).build();
+    TestWorkflow1 workflow = client.newWorkflowStub(TestWorkflow1.class, workflowOptions);
+
+    String message = workflow.execute("input1");
+    assertEquals("Execute-input1", message);
+  }
+
+  static boolean isEncoded(Payload payload) {
+    return payload.getData().startsWith(PrefixPayloadCodec.PREFIX);
   }
 
   private static final class PrefixPayloadCodec implements PayloadCodec {
@@ -201,6 +304,33 @@ public class FailureConverterTest {
         throw new PayloadCodecException("Payload is not correctly encoded");
       ByteString decodedData = encodedData.substring(PREFIX.size());
       return encodedPayload.toBuilder().setData(decodedData).build();
+    }
+  }
+
+  private static class FailingActivityImpl implements TestActivities.TestActivity1 {
+
+    @Override
+    public String execute(String input) {
+      throw new IllegalThreadStateException(
+          Activity.getExecutionContext().getInfo().getActivityType() + "-" + input);
+    }
+  }
+
+  public static class FailingActivityWorkflow implements TestWorkflow1 {
+
+    private final TestActivities.TestActivity1 activity =
+        Workflow.newActivityStub(
+            TestActivities.TestActivity1.class,
+            ActivityOptions.newBuilder().setScheduleToCloseTimeout(Duration.ofSeconds(2)).build());
+
+    @Override
+    public String execute(String input) {
+      try {
+        activity.execute(input);
+        return "DID NOT FAILED";
+      } catch (ActivityFailure e) {
+        return ((TemporalFailure) e.getCause()).getOriginalMessage();
+      }
     }
   }
 }
