3d100897bfb8b83ea3e8a6fed41eabcf1f0f3352
Frank Shaw <frank.shaw@zoominfo.com>
2023-04-28 21:38:47 -0700

    Allow task queue names to use property placeholders
    To enable more dynamic usage when utilizing the @WorkflowImpl and @ActivityImpl
    annotations, along with the workers-auto-discovery feature, this allows the
    task queue name values to be Spring property placeholder that will be resolved
    and populated by Spring at runtime.

---
+++ temporal-spring-boot-autoconfigure-alpha/src/test/java/io/temporal/spring/boot/autoconfigure/AutoDiscoveryByTaskQueueResolverTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2022 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ * Copyright (C) 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this material except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.temporal.spring.boot.autoconfigure;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.spring.boot.autoconfigure.bytaskqueue.TestWorkflow;
+import io.temporal.worker.WorkerFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = AutoDiscoveryByTaskQueueResolverTest.Configuration.class)
+@ActiveProfiles(profiles = "auto-discovery-by-task-queue-dynamic-suffix")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AutoDiscoveryByTaskQueueResolverTest {
+  @Autowired ConfigurableApplicationContext applicationContext;
+
+  @Autowired WorkflowClient workflowClient;
+
+  @Autowired WorkerFactory workerFactory;
+
+  @BeforeEach
+  void setUp() {
+    applicationContext.start();
+  }
+
+  @Test
+  @Timeout(value = 10)
+  public void testAutoDiscovery() {
+    TestWorkflow testWorkflow =
+        workflowClient.newWorkflowStub(
+            TestWorkflow.class,
+            WorkflowOptions.newBuilder().setTaskQueue("PropertyResolverTest").build());
+    testWorkflow.execute("input");
+  }
+
+  @ComponentScan(
+      excludeFilters =
+          @ComponentScan.Filter(
+              pattern = "io\\.temporal\\.spring\\.boot\\.autoconfigure\\.byworkername\\..*",
+              type = FilterType.REGEX))
+  public static class Configuration {}
+}
