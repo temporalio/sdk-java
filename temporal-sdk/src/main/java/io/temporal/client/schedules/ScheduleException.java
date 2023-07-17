9ecfd2efe1dbda1deb7d134184224f5ed9a6a2f2
Bryan Maloyer <bryan.mlyr@gmail.com>
2023-07-17 19:10:42 +0200

    Create ScheduleException.java

---
+++ temporal-sdk/src/main/java/io/temporal/client/schedules/ScheduleException.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ * Copyright (C) 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this material except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.temporal.client.schedules;
+
+import io.temporal.failure.TemporalException;
+
+/** Exception thrown by client when attempting to create a schedule. */
+public final class ScheduleException extends TemporalException {
+  public ScheduleException(Throwable cause) {
+    super("Schedule exception", cause);
+  }
+}
