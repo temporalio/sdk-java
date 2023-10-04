e5d0da100c7f10c0678adf4c6f7e12803dc513f9
Quinn Klassen <klassenq@gmail.com>
2023-10-04 10:52:23 -0700

    Use ApplicationReadyEvent to start spring workers

--- temporal-spring-boot-autoconfigure-alpha/src/main/java/io/temporal/spring/boot/autoconfigure/RootNamespaceAutoConfiguration.java
+++ temporal-spring-boot-autoconfigure-alpha/src/main/java/io/temporal/spring/boot/autoconfigure/RootNamespaceAutoConfiguration.java
@@ -47,13 +47,13 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.event.ContextStartedEvent;
 
 @Configuration
 @EnableConfigurationProperties(TemporalProperties.class)
@@ -159,15 +159,7 @@ public class RootNamespaceAutoConfiguration {
     return new WorkerFactoryStarter(workerFactory);
   }
 
-  // It needs to listed on ContextStartedEvent, not ContextRefreshedEvent.
-  // Using ContextRefreshedEvent will cause start of workers early, during the context
-  // initialization
-  // and potentially incorrect order of bean initialization.
-  // Refresh event can also be fired multiple times during the context initialization.
-  // For this listener to ever work, Spring context needs to be actually started.
-  // Note that a lot of online samples for Spring don't start the context at all:
-  // https://stackoverflow.com/questions/48099355/contextstartedevent-not-firing-in-custom-listener
-  public static class WorkerFactoryStarter implements ApplicationListener<ContextStartedEvent> {
+  public static class WorkerFactoryStarter implements ApplicationListener<ApplicationReadyEvent> {
     private final WorkerFactory workerFactory;
 
     public WorkerFactoryStarter(WorkerFactory workerFactory) {
@@ -175,7 +167,7 @@ public class RootNamespaceAutoConfiguration {
     }
 
     @Override
-    public void onApplicationEvent(@Nonnull ContextStartedEvent event) {
+    public void onApplicationEvent(@Nonnull ApplicationReadyEvent event) {
       workerFactory.start();
     }
   }
