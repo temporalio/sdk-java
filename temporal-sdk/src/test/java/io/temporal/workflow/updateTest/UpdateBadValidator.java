8af4a2647dacab2cb0d4a3a613482e00bc297a39
Quinn Klassen <klassenq@gmail.com>
2023-10-30 15:27:51 -0700

    ReadOnly Exception in update validator fail WFT (#1918)
    ReadOnly Exception in update validator fail WFT

--- temporal-sdk/src/test/java/io/temporal/workflow/updateTest/UpdateBadValidator.java
+++ temporal-sdk/src/test/java/io/temporal/workflow/updateTest/UpdateBadValidator.java
@@ -21,7 +21,6 @@
 package io.temporal.workflow.updateTest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 
 import io.temporal.activity.*;
 import io.temporal.api.common.v1.WorkflowExecution;
@@ -33,6 +32,7 @@ import io.temporal.workflow.CompletablePromise;
 import io.temporal.workflow.Workflow;
 import io.temporal.workflow.shared.TestActivities;
 import io.temporal.workflow.shared.TestWorkflows;
+import java.time.Duration;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.Rule;
@@ -41,6 +41,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UpdateBadValidator {
+  private static int testWorkflowTaskFailureReplayCount;
 
   private static final Logger log = LoggerFactory.getLogger(UpdateTest.class);
 
@@ -51,13 +52,14 @@ public class UpdateBadValidator {
           .setWorkflowTypes(TestUpdateWithBadValidatorWorkflowImpl.class)
           .build();
 
-  @Test
+  @Test(timeout = 30000)
   public void testBadUpdateValidator() {
     String workflowId = UUID.randomUUID().toString();
     WorkflowClient workflowClient = testWorkflowRule.getWorkflowClient();
     WorkflowOptions options =
         SDKTestOptions.newWorkflowOptionsWithTimeouts(testWorkflowRule.getTaskQueue()).toBuilder()
             .setWorkflowId(workflowId)
+            .setWorkflowTaskTimeout(Duration.ofSeconds(1))
             .build();
     TestWorkflows.WorkflowWithUpdate workflow =
         workflowClient.newWorkflowStub(TestWorkflows.WorkflowWithUpdate.class, options);
@@ -69,9 +71,9 @@ public class UpdateBadValidator {
     assertEquals("initial", workflow.getState());
 
     assertEquals(workflowId, execution.getWorkflowId());
-
     for (String testCase : TestWorkflows.illegalCallCases) {
-      assertThrows(WorkflowUpdateException.class, () -> workflow.update(0, testCase));
+      assertEquals("2", workflow.update(0, testCase));
+      testWorkflowTaskFailureReplayCount = 0;
     }
 
     workflow.complete();
@@ -102,12 +104,15 @@ public class UpdateBadValidator {
 
     @Override
     public String update(Integer index, String value) {
-      return "";
+      return String.valueOf(testWorkflowTaskFailureReplayCount);
     }
 
     @Override
     public void updateValidator(Integer index, String testCase) {
-      TestWorkflows.illegalCalls(testCase);
+      if (testWorkflowTaskFailureReplayCount < 2) {
+        testWorkflowTaskFailureReplayCount += 1;
+        TestWorkflows.illegalCalls(testCase);
+      }
     }
 
     @Override
