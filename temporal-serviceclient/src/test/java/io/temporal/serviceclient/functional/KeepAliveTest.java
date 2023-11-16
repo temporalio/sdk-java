499593f4150b00c28f0694bc48837f36ab6e47a6
Quinn Klassen <klassenq@gmail.com>
2023-11-16 08:00:28 -0800

    Enable TCP keep alive by default (#1873)
    Enable TCP keep alive by default

---
+++ temporal-serviceclient/src/test/java/io/temporal/serviceclient/functional/KeepAliveTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2022 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ * Copyright (C) 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this material except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.temporal.serviceclient.functional;
+
+import static org.junit.Assert.assertEquals;
+
+import io.temporal.serviceclient.WorkflowServiceStubsOptions;
+import io.temporal.testing.internal.SDKTestWorkflowRule;
+import java.time.Duration;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class KeepAliveTest {
+  @Rule public SDKTestWorkflowRule testWorkflowRule = SDKTestWorkflowRule.newBuilder().build();
+
+  @Test
+  public void testKeepAliveOnByDefault() {
+    WorkflowServiceStubsOptions options = testWorkflowRule.getWorkflowServiceStubs().getOptions();
+    assertEquals(true, options.getEnableKeepAlive());
+    assertEquals(true, options.getKeepAlivePermitWithoutStream());
+    assertEquals(Duration.ofSeconds(30), options.getKeepAliveTime());
+    assertEquals(Duration.ofSeconds(15), options.getKeepAliveTimeout());
+  }
+}
