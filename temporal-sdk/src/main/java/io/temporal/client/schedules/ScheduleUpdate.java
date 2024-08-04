346e55bbb70fe6433095b3a489a03731c71f50e2
Quinn Klassen <klassenq@gmail.com>
2024-08-03 17:48:45 -0700

    Fix tests

--- temporal-sdk/src/main/java/io/temporal/client/schedules/ScheduleUpdate.java
+++ temporal-sdk/src/main/java/io/temporal/client/schedules/ScheduleUpdate.java
@@ -27,11 +27,23 @@ public final class ScheduleUpdate {
   private final Schedule schedule;
   private final SearchAttributes typedSearchAttributes;
 
+  /**
+   * Create a new ScheduleUpdate.
+   *
+   * @param schedule schedule to replace the existing schedule with
+   */
   public ScheduleUpdate(Schedule schedule) {
     this.schedule = schedule;
     this.typedSearchAttributes = null;
   }
 
+  /**
+   * Create a new ScheduleUpdate.
+   *
+   * @param schedule schedule to replace the existing schedule with
+   * @param typedSearchAttributes search attributes to replace the existing search attributes with.
+   *     Returning null will not update the search attributes.
+   */
   public ScheduleUpdate(Schedule schedule, SearchAttributes typedSearchAttributes) {
     this.schedule = schedule;
     this.typedSearchAttributes = typedSearchAttributes;
