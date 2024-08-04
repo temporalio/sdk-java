bcfb1fae8dca5213a03e21f1b6bd4b0824a732da
Quinn Klassen <klassenq@gmail.com>
2024-08-03 17:50:12 -0700

    Add copyright header

--- temporal-sdk/src/test/java/io/temporal/testUtils/Eventually.java
+++ temporal-sdk/src/test/java/io/temporal/testUtils/Eventually.java
@@ -1,39 +1,52 @@
+/*
+ * Copyright (C) 2022 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ * Copyright (C) 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this material except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.temporal.testUtils;
 
 import java.time.Duration;
 import java.time.Instant;
 
 public class Eventually {
+  public static void assertEventually(Duration timeout, Runnable command) {
+    final Instant start = Instant.now();
+    final Instant deadline = start.plus(timeout);
 
-    /**
-     * @param maxDuration the duration during which the command will be retried after it fails exceptionally
-     * @param command       the logic that fails exceptionally while its assertions are not successful
-     */
-    public static void assertEventually(Duration maxDuration, Runnable command) {
-        final Instant start = Instant.now();
-        final Instant max = start.plus(maxDuration);
-
-        boolean failed;
-        do {
-            try {
-                command.run();
-                failed = false;
-            } catch (Throwable t) {
-                failed = true;
-                if (Instant.now().isBefore(max)) {
-                    // Try again after a short nap
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        throw new RuntimeException(e);
-                    }
-                } else {
-                    // Max duration has exceeded, it took too long to become consistent
-                    throw t;
-                }
-            }
-        } while (failed);
-    }
-
+    boolean failed;
+    do {
+      try {
+        command.run();
+        failed = false;
+      } catch (Throwable t) {
+        failed = true;
+        if (Instant.now().isBefore(deadline)) {
+          // Try again after a short nap
+          try {
+            Thread.sleep(100);
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+          }
+        } else {
+          throw t;
+        }
+      }
+    } while (failed);
+  }
 }
