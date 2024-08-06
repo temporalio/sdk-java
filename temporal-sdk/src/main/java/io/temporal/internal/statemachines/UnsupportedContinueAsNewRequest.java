fc63989b4b9f899ea05bc9c3d049ee6d7eeec40b
Quinn Klassen <klassenq@gmail.com>
2024-08-06 08:35:12 -0700

    Fixup docs

--- temporal-sdk/src/main/java/io/temporal/internal/statemachines/UnsupportedContinueAsNewRequest.java
+++ temporal-sdk/src/main/java/io/temporal/internal/statemachines/UnsupportedContinueAsNewRequest.java
@@ -21,8 +21,8 @@
 package io.temporal.internal.statemachines;
 
 /**
- * Thrown when {@link io.temporal.workflow.Workflow#continueAsNew} is called from a location that is
- * not supported.
+ * Thrown when {@link io.temporal.workflow.Workflow#continueAsNew} is called from an unsupported
+ * location.
  *
  * <p>The reason this class extends Error is for application workflow code to not catch it by
  * mistake. The default behavior of the SDK is to block workflow execution while Error is thrown.
