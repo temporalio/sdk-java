f6735e4fe887803391eb150dcf54f674d1e31d57
Quinn Klassen <klassenq@gmail.com>
2024-08-06 09:44:01 -0700

    Add docs

--- temporal-sdk/src/main/java/io/temporal/client/WorkflowUpdateTimeoutOrCancelledException.java
+++ temporal-sdk/src/main/java/io/temporal/client/WorkflowUpdateTimeoutOrCancelledException.java
@@ -22,6 +22,12 @@ package io.temporal.client;
 
 import io.temporal.api.common.v1.WorkflowExecution;
 
+/**
+ *  Error that occurs when an update call times out or is cancelled.
+ *  
+ *  <p>Note, this is not related to any general concept of timing out or cancelling
+ *  a running update, this is only related to the client call itself.
+ */
 public class WorkflowUpdateTimeoutOrCancelledException extends WorkflowServiceException {
   public WorkflowUpdateTimeoutOrCancelledException(
       WorkflowExecution execution, String updateId, String updateName, Throwable cause) {
