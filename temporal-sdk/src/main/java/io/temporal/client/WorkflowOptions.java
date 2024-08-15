6e77d7bac92efd6129a95e646c361427a384620b
Quinn Klassen <klassenq@gmail.com>
2024-08-15 16:21:35 -0700

    Fix start delay docs

--- temporal-sdk/src/main/java/io/temporal/client/WorkflowOptions.java
+++ temporal-sdk/src/main/java/io/temporal/client/WorkflowOptions.java
@@ -374,9 +374,8 @@ public final class WorkflowOptions {
     }
 
     /**
-     * Time to wait before dispatching the first workflow task. If the workflow gets a signal before
-     * the delay, a workflow task will be dispatched and the rest of the delay will be ignored. A
-     * signal from signal with start will not trigger a workflow task. Cannot be set the same time
+     * Time to wait before dispatching the first workflow task.
+     *  A signal from signal with start will not trigger a workflow task. Cannot be set the same time
      * as a CronSchedule.
      */
     public Builder setStartDelay(Duration startDelay) {
