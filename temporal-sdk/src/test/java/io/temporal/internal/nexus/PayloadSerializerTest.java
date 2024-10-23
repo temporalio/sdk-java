1a1a474555b12c4bbe97827d47edd4bce1532b75
Quinn Klassen <klassenq@gmail.com>
2024-10-23 15:22:02 -0700

    Fix

--- temporal-sdk/src/test/java/io/temporal/internal/nexus/PayloadSerializerTest.java
+++ temporal-sdk/src/test/java/io/temporal/internal/nexus/PayloadSerializerTest.java
@@ -20,9 +20,11 @@
 
 package io.temporal.internal.nexus;
 
+import com.google.common.reflect.TypeToken;
 import io.temporal.api.common.v1.WorkflowExecution;
 import io.temporal.common.converter.DataConverter;
 import io.temporal.common.converter.DefaultDataConverter;
+import io.temporal.common.converter.EncodedValuesTest;
 import java.util.Collections;
 import java.util.Map;
 import org.junit.Assert;
@@ -42,23 +44,33 @@ public class PayloadSerializerTest {
   @Test
   public void testNull() {
     PayloadSerializer.Content content = payloadSerializer.serialize(null);
-    payloadSerializer.deserialize(content, String.class);
     Assert.assertEquals(null, payloadSerializer.deserialize(content, String.class));
   }
 
   @Test
   public void testInteger() {
     PayloadSerializer.Content content = payloadSerializer.serialize(1);
-    payloadSerializer.deserialize(content, Integer.class);
     Assert.assertEquals(1, payloadSerializer.deserialize(content, Integer.class));
   }
 
   @Test
+  public void testArray() {
+    String[] cars = {"test", "nexus", "serialization"};
+    PayloadSerializer.Content content = payloadSerializer.serialize(cars);
+    Assert.assertArrayEquals(
+        cars, (String[]) payloadSerializer.deserialize(content, String[].class));
+  }
+
+  @Test
   public void testHashMap() {
-    Map<String, Integer> map = Collections.singletonMap("key", 0);
+    Map<String, EncodedValuesTest.Pair> map =
+        Collections.singletonMap("key", new EncodedValuesTest.Pair(1, "hello"));
     PayloadSerializer.Content content = payloadSerializer.serialize(map);
-    payloadSerializer.deserialize(content, map.getClass());
-    Assert.assertEquals(map, payloadSerializer.deserialize(content, map.getClass()));
+    Map<String, EncodedValuesTest.Pair> newMap =
+        (Map<String, EncodedValuesTest.Pair>)
+            payloadSerializer.deserialize(
+                content, (new TypeToken<Map<String, EncodedValuesTest.Pair>>() {}).getType());
+    Assert.assertTrue(newMap.get("key") instanceof EncodedValuesTest.Pair);
   }
 
   @Test
@@ -66,7 +78,6 @@ public class PayloadSerializerTest {
     WorkflowExecution exec =
         WorkflowExecution.newBuilder().setWorkflowId("id").setRunId("runId").build();
     PayloadSerializer.Content content = payloadSerializer.serialize(exec);
-    payloadSerializer.deserialize(content, WorkflowExecution.class);
     Assert.assertEquals(exec, payloadSerializer.deserialize(content, WorkflowExecution.class));
   }
 }
