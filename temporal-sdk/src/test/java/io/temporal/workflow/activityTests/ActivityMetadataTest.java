0253534f9fe1660231f9ba4fb613dae70579c1bd
Quinn Klassen <klassenq@gmail.com>
2024-11-04 07:51:39 -0800

    Add summary metadata to Activity

---
+++ temporal-sdk/src/test/java/io/temporal/workflow/activityTests/ActivityMetadataTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2022 Temporal Technologies, Inc. All Rights Reserved.
+ *
+ * Copyright (C) 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this material except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.temporal.workflow.activityTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
+
+import io.temporal.activity.ActivityOptions;
+import io.temporal.api.history.v1.HistoryEvent;
+import io.temporal.common.WorkflowExecutionHistory;
+import io.temporal.common.converter.DefaultDataConverter;
+import io.temporal.testing.internal.SDKTestWorkflowRule;
+import io.temporal.workflow.Workflow;
+import io.temporal.workflow.shared.TestActivities;
+import io.temporal.workflow.shared.TestWorkflows.TestWorkflow1;
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ActivityMetadataTest {
+
+  @Rule
+  public SDKTestWorkflowRule testWorkflowRule =
+      SDKTestWorkflowRule.newBuilder()
+          .setWorkflowTypes(TestWorkflowImpl.class)
+          .setActivityImplementations(new TestActivities.TestActivitiesImpl())
+          .build();
+
+  static final String activitySummary = "activity-summary";
+
+  @Before
+  public void checkRealServer() {
+    assumeTrue("skipping for test server", SDKTestWorkflowRule.useExternalService);
+  }
+
+  @Test
+  public void testActivityWithMetaData() {
+    TestWorkflow1 stub = testWorkflowRule.newWorkflowStubTimeoutOptions(TestWorkflow1.class);
+
+    String workflowId = stub.execute(testWorkflowRule.getTaskQueue());
+
+    WorkflowExecutionHistory workflowExecutionHistory =
+        testWorkflowRule.getWorkflowClient().fetchHistory(workflowId);
+    List<HistoryEvent> activityScheduledEvents =
+        workflowExecutionHistory.getEvents().stream()
+            .filter(HistoryEvent::hasActivityTaskScheduledEventAttributes)
+            .collect(Collectors.toList());
+    assertEventMetadata(activityScheduledEvents.get(0), activitySummary, null);
+  }
+
+  private void assertEventMetadata(HistoryEvent event, String summary, String details) {
+    if (summary != null) {
+      String describedSummary =
+          DefaultDataConverter.STANDARD_INSTANCE.fromPayload(
+              event.getUserMetadata().getSummary(), String.class, String.class);
+      assertEquals(summary, describedSummary);
+    }
+    if (details != null) {
+      String describedDetails =
+          DefaultDataConverter.STANDARD_INSTANCE.fromPayload(
+              event.getUserMetadata().getDetails(), String.class, String.class);
+      assertEquals(details, describedDetails);
+    }
+  }
+
+  public static class TestWorkflowImpl implements TestWorkflow1 {
+
+    private final TestActivities.VariousTestActivities activities =
+        Workflow.newActivityStub(
+            TestActivities.VariousTestActivities.class,
+            ActivityOptions.newBuilder()
+                .setSummary(activitySummary)
+                .setStartToCloseTimeout(Duration.ofSeconds(5))
+                .build());
+
+    @Override
+    public String execute(String taskQueue) {
+      return activities.activity();
+    }
+  }
+}
