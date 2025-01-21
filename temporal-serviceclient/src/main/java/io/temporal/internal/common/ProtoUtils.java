b3111b51ae85fdf5f883344274a82a57b8e14fa7
Quinn Klassen <klassenq@gmail.com>
2025-01-21 07:34:51 -0800

    Support Proto 3 and 4

--- temporal-serviceclient/src/main/java/io/temporal/internal/common/ProtoUtils.java
+++ temporal-serviceclient/src/main/java/io/temporal/internal/common/ProtoUtils.java
@@ -28,8 +28,7 @@ public class ProtoUtils {
    * This method does exactly what {@link Any#pack(Message)} does. But it doesn't go into reflection
    * to fetch the {@code descriptor}, which allows us to avoid a bunch of Graal reflection configs.
    */
-  public static <T extends GeneratedMessageV3> Any packAny(
-      T details, Descriptors.Descriptor descriptor) {
+  public static <T extends Message> Any packAny(T details, Descriptors.Descriptor descriptor) {
     return Any.newBuilder()
         .setTypeUrl("type.googleapis.com/" + descriptor.getFullName())
         .setValue(details.toByteString())
