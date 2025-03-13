0ee60c0420aea050fad370c501f269553baa40b0
Rodrigo Zhou <2068124+rodrigozhou@users.noreply.github.com>
2025-03-13 14:15:59 -0500

    Unblock UseExisting conflict policy for Nexux WorkflowRunOperation (#2440)

--- temporal-sdk/src/test/java/io/temporal/workflow/nexus/WorkflowHandleUseExistingOnConflictTest.java
+++ temporal-sdk/src/test/java/io/temporal/workflow/nexus/WorkflowHandleUseExistingOnConflictTest.java
@@ -36,7 +36,6 @@ import java.util.List;
 import java.util.UUID;
 import org.junit.*;
 
-@Ignore("Skipping until we can support USE_EXISTING")
 public class WorkflowHandleUseExistingOnConflictTest {
   @Rule
   public SDKTestWorkflowRule testWorkflowRule =
