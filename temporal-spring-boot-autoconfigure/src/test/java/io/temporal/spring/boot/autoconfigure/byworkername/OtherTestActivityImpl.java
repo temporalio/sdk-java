0e8ac3569c239895626d5aa35fb07a498cff969d
Quinn Klassen <klassenq@gmail.com>
2025-05-05 09:08:01 -0700

    Make @Profile annotation work (#2501)
    Make @Profile annotation work

--- temporal-spring-boot-autoconfigure/src/test/java/io/temporal/spring/boot/autoconfigure/byworkername/OtherTestActivityImpl.java
+++ temporal-spring-boot-autoconfigure/src/test/java/io/temporal/spring/boot/autoconfigure/byworkername/OtherTestActivityImpl.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Component;
 
 @Component("TestActivityImpl")
 @ActivityImpl(workers = "mainWorker")
-@Profile("!auto-discovery-with-profile")
-public class TestActivityImpl implements TestActivity {
+@Profile("auto-discovery-with-profile")
+public class OtherTestActivityImpl implements TestActivity {
   @Override
   public String execute(String input) {
-    return input;
+    return "other workflow " + input;
   }
 }
--- temporal-spring-boot-autoconfigure/src/test/java/io/temporal/spring/boot/autoconfigure/byworkername/OtherTestWorkflowImpl.java
+++ temporal-spring-boot-autoconfigure/src/test/java/io/temporal/spring/boot/autoconfigure/byworkername/OtherTestWorkflowImpl.java
@@ -2,40 +2,26 @@ package io.temporal.spring.boot.autoconfigure.byworkername;
 
 import io.temporal.activity.ActivityOptions;
 import io.temporal.spring.boot.WorkflowImpl;
-import io.temporal.workflow.NexusOperationOptions;
-import io.temporal.workflow.NexusServiceOptions;
 import io.temporal.workflow.Workflow;
 import java.time.Duration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Profile;
 
 @WorkflowImpl(workers = "mainWorker")
-@Profile("!auto-discovery-with-profile")
-public class TestWorkflowImpl implements TestWorkflow {
+@Profile("auto-discovery-with-profile")
+public class OtherTestWorkflowImpl implements TestWorkflow {
 
   // Test auto-wiring of the application context works, this is not indicative of a real-world use
   // case as the workflow implementation should be stateless.
-  public TestWorkflowImpl(ConfigurableApplicationContext applicationContext) {}
+  public OtherTestWorkflowImpl(ConfigurableApplicationContext applicationContext) {}
 
   @Override
   public String execute(String input) {
-    if (input.equals("nexus")) {
-      Workflow.newNexusServiceStub(
-              TestNexusService.class,
-              NexusServiceOptions.newBuilder()
-                  .setEndpoint("AutoDiscoveryByWorkerNameTestEndpoint")
-                  .setOperationOptions(
-                      NexusOperationOptions.newBuilder()
-                          .setScheduleToCloseTimeout(Duration.ofSeconds(10))
-                          .build())
-                  .build())
-          .operation(input);
-    }
     return Workflow.newActivityStub(
             TestActivity.class,
             ActivityOptions.newBuilder()
                 .setStartToCloseTimeout(Duration.ofSeconds(1))
                 .validateAndBuildWithDefaults())
-        .execute("done");
+        .execute("discovered");
   }
 }
