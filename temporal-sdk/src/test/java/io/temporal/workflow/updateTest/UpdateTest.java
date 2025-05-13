fd33162e1c6c4902d511b6cbd54cf9c3addb4544
Quinn Klassen <klassenq@gmail.com>
2025-05-12 22:41:09 -0700

    fix compile

--- temporal-sdk/src/test/java/io/temporal/workflow/updateTest/UpdateTest.java
+++ temporal-sdk/src/test/java/io/temporal/workflow/updateTest/UpdateTest.java
@@ -31,6 +31,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
