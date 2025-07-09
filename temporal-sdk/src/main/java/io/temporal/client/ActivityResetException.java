d310594f76a9c7d0d842f0410b36564c77e56cf4
Quinn Klassen <klassenq@gmail.com>
2025-07-08 23:26:00 -0700

    Add support for activity reset (#2546)
    Add support for activity reset

--- temporal-sdk/src/main/java/io/temporal/client/ActivityResetException.java
+++ temporal-sdk/src/main/java/io/temporal/client/ActivityResetException.java
@@ -4,17 +4,17 @@ import io.temporal.activity.ActivityInfo;
 import io.temporal.common.Experimental;
 
 /***
- * Indicates that the activity was paused by the user.
+ * Indicates that the activity attempt was reset by the user.
  *
  * <p>Catching this exception directly is discouraged and catching the parent class {@link ActivityCompletionException} is recommended instead.<br>
  */
 @Experimental
-public final class ActivityPausedException extends ActivityCompletionException {
-  public ActivityPausedException(ActivityInfo info) {
+public final class ActivityResetException extends ActivityCompletionException {
+  public ActivityResetException(ActivityInfo info) {
     super(info);
   }
 
-  public ActivityPausedException() {
+  public ActivityResetException() {
     super();
   }
 }
