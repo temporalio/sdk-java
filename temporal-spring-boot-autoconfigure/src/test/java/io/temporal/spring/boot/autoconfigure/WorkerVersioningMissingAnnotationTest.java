c040f0da3ddf3698be5588bd205341e664f580ef
Spencer Judge <spencer@temporal.io>
2025-10-15 09:37:48 -0700

    Add separate deployment name and build id fields for spring (#2681)

--- temporal-spring-boot-autoconfigure/src/test/java/io/temporal/spring/boot/autoconfigure/WorkerVersioningMissingAnnotationTest.java
+++ temporal-spring-boot-autoconfigure/src/test/java/io/temporal/spring/boot/autoconfigure/WorkerVersioningMissingAnnotationTest.java
@@ -29,6 +29,39 @@ public class WorkerVersioningMissingAnnotationTest {
     assertThat(e).hasMessageContaining("must have a VersioningBehavior set");
   }
 
+  @Test
+  void testFailsWithMissingBuildId() {
+    BeanCreationException e =
+        assertThrows(
+            BeanCreationException.class,
+            () -> {
+              try (ConfigurableApplicationContext ignored =
+                  new SpringApplicationBuilder(Configuration.class)
+                      .profiles("worker-versioning-need-both-deployname-buildid")
+                      .run()) {
+                fail("Should not load");
+              }
+            });
+    assertThat(e).hasMessageContaining("deploymentName and buildId must both be set");
+  }
+
+  @Test
+  void testFailsWithBothVersionOptions() {
+    BeanCreationException e =
+        assertThrows(
+            BeanCreationException.class,
+            () -> {
+              try (ConfigurableApplicationContext ignored =
+                  new SpringApplicationBuilder(Configuration.class)
+                      .profiles("worker-versioning-cant-use-old-version-and-new")
+                      .run()) {
+                fail("Should not load");
+              }
+            });
+    assertThat(e)
+        .hasMessageContaining("deploymentVersion is exclusive with deploymentName and buildId");
+  }
+
   @ComponentScan(
       excludeFilters =
           @ComponentScan.Filter(
