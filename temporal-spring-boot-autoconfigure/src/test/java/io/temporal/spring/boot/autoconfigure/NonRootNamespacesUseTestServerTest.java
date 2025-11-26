0bfa9103bd7888b24f204cfaa44503dbbe469467
Bill Richards <bill.richards@temporal.io>
2025-11-26 09:39:03 -0800

    fix(spring-boot): use in-memory test server for non-root namespaces (#2736)
    * fix(spring-boot): use in-memory test server for non-root namespaces\n\nFixes #2735.\n\n- Resolve TestWorkflowEnvironmentAdapter by type, fallback to correct bean name\n- Add regression test ensuring non-root namespaces use in-memory env even with connection.target set\n- Format code via spotlessApply
    
    * test(spring-boot): avoid javax.annotation.Resource in test to fix CI

---
+++ temporal-spring-boot-autoconfigure/src/test/java/io/temporal/spring/boot/autoconfigure/NonRootNamespacesUseTestServerTest.java
@@ -0,0 +1,63 @@
+package io.temporal.spring.boot.autoconfigure;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.grpc.health.v1.HealthCheckResponse;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import io.temporal.testing.TestWorkflowEnvironment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+@SpringBootTest(
+    classes = NonRootNamespacesUseTestServerTest.Configuration.class,
+    properties = {
+      "spring.temporal.test-server.enabled=true",
+      "spring.temporal.connection.target=127.0.0.1:7233",
+      "spring.temporal.start-workers=false",
+      "spring.temporal.namespaces[0].namespace=pomegranate",
+      "spring.temporal.namespaces[0].alias=pomegranate"
+    })
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class NonRootNamespacesUseTestServerTest {
+
+  @Autowired ConfigurableApplicationContext applicationContext;
+
+  @Autowired
+  @Qualifier("temporalTestWorkflowEnvironment")
+  TestWorkflowEnvironment testWorkflowEnvironment;
+
+  @Autowired
+  @Qualifier("pomegranateWorkflowServiceStubs")
+  WorkflowServiceStubs pomegranateWorkflowServiceStubs;
+
+  @BeforeEach
+  void setUp() {
+    applicationContext.start();
+  }
+
+  @Test
+  @Timeout(10)
+  public void nonRootNamespaceUsesInMemoryTestServer() {
+    HealthCheckResponse envHealth =
+        testWorkflowEnvironment.getWorkflowClient().getWorkflowServiceStubs().healthCheck();
+    HealthCheckResponse pomegranateHealth = pomegranateWorkflowServiceStubs.healthCheck();
+    assertEquals(HealthCheckResponse.ServingStatus.SERVING, envHealth.getStatus());
+    assertEquals(HealthCheckResponse.ServingStatus.SERVING, pomegranateHealth.getStatus());
+  }
+
+  @ComponentScan(
+      excludeFilters =
+          @ComponentScan.Filter(
+              pattern =
+                  "io\\.temporal\\.spring\\.boot\\.autoconfigure\\.(byworkername|bytaskqueue)\\..*",
+              type = FilterType.REGEX))
+  public static class Configuration {}
+}
