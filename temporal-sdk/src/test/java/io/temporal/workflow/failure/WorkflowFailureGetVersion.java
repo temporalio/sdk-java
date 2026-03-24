b4e38bfaba196b986060da53f9f0d10ba7a62965
Edward Amsden <edward.amsden@temporal.io>
2026-03-24 16:46:52 -0500

    Repro as test for #2810

---
+++ temporal-sdk/src/test/java/io/temporal/workflow/failure/WorkflowFailureGetVersion.java
@@ -0,0 +1,69 @@
+package io.temporal.workflow.failure;
+
+import io.temporal.client.WorkflowException;
+import io.temporal.common.RetryOptions;
+import io.temporal.failure.ApplicationFailure;
+import io.temporal.testing.internal.SDKTestOptions;
+import io.temporal.testing.internal.SDKTestWorkflowRule;
+import io.temporal.workflow.Workflow;
+import io.temporal.workflow.shared.TestWorkflows.TestWorkflow1;
+import java.time.Duration;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class WorkflowFailureGetVersion {
+
+  @Rule public TestName testName = new TestName();
+
+  @Rule
+  public SDKTestWorkflowRule testWorkflowRule =
+      SDKTestWorkflowRule.newBuilder()
+          .setWorkflowTypes(TestWorkflowGetVersionAndException.class)
+          .build();
+
+  @Test
+  public void getVersionAndException() {
+    RetryOptions workflowRetryOptions =
+        RetryOptions.newBuilder()
+            .setInitialInterval(Duration.ofSeconds(1))
+            .setMaximumAttempts(100)
+            .setBackoffCoefficient(1.0)
+            .build();
+    TestWorkflow1 workflowStub =
+        testWorkflowRule
+            .getWorkflowClient()
+            .newWorkflowStub(
+                TestWorkflow1.class,
+                SDKTestOptions.newWorkflowOptionsWithTimeouts(testWorkflowRule.getTaskQueue())
+                    .toBuilder()
+                    .setRetryOptions(workflowRetryOptions)
+                    .build());
+    try {
+      workflowStub.execute(testName.getMethodName());
+      Assert.fail("unreachable");
+    } catch (WorkflowException e) {
+      Assert.assertTrue(e.getCause() instanceof ApplicationFailure);
+      Assert.assertEquals("foo", ((ApplicationFailure) e.getCause()).getType());
+      Assert.assertEquals(
+          "details1", ((ApplicationFailure) e.getCause()).getDetails().get(0, String.class));
+      Assert.assertEquals(
+          Integer.valueOf(123),
+          ((ApplicationFailure) e.getCause()).getDetails().get(1, Integer.class));
+      Assert.assertEquals(
+          "message='simulated 3', type='foo', nonRetryable=true", e.getCause().getMessage());
+    }
+  }
+
+  public static class TestWorkflowGetVersionAndException implements TestWorkflow1 {
+
+    @Override
+    public String execute(String unused) {
+      String changeId = "change-id";
+      Workflow.getVersion(changeId, Workflow.DEFAULT_VERSION, 1);
+      Workflow.getVersion(changeId, Workflow.DEFAULT_VERSION, 1);
+      throw new RuntimeException("Any error");
+    }
+  }
+}
