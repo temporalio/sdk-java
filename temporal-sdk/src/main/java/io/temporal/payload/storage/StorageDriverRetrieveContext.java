f434a516c13a6eb5c07be394123d6b56d417222d
Chris Constable <chris.constable@temporal.io>
2026-06-08 12:34:50 -0400

    Refactor storage driver context to be interface.

--- temporal-sdk/src/main/java/io/temporal/payload/storage/StorageDriverRetrieveContext.java
+++ temporal-sdk/src/main/java/io/temporal/payload/storage/StorageDriverRetrieveContext.java
@@ -2,6 +2,11 @@ package io.temporal.payload.storage;
 
 import io.temporal.common.Experimental;
 
-/** Context passed to {@link StorageDriver#retrieve}. */
+/**
+ * Context passed to {@link StorageDriver#retrieve}.
+ *
+ * <p>Implemented by the SDK and passed to the driver. Driver authors do not implement this in
+ * production code, only when constructing instances for their own tests.
+ */
 @Experimental
-public final class StorageDriverRetrieveContext {}
+public interface StorageDriverRetrieveContext {}
