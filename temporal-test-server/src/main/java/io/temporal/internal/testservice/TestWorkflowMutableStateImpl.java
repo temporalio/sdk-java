f6ca6adc3b5381b9d4e1d4f067e64598334119d8
Greg Travis <greg.m.travis@gmail.com>
2026-06-10 10:22:19 -0400

    fqn not needed

--- temporal-test-server/src/main/java/io/temporal/internal/testservice/TestWorkflowMutableStateImpl.java
+++ temporal-test-server/src/main/java/io/temporal/internal/testservice/TestWorkflowMutableStateImpl.java
@@ -622,7 +622,7 @@ class TestWorkflowMutableStateImpl implements TestWorkflowMutableState {
   public void applyOnConflictOptions(@Nonnull StartWorkflowExecutionRequest request) {
     update(
         ctx -> {
-          io.temporal.api.workflow.v1.OnConflictOptions options = request.getOnConflictOptions();
+          OnConflictOptions options = request.getOnConflictOptions();
           String requestId = null;
           List<Callback> completionCallbacks = null;
           List<Link> links = null;
