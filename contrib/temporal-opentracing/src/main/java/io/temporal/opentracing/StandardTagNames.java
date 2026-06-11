ee62c8f3fac070fc8931fc9515e01ea3778a60d2
444am <at444am@gmail.com>
2026-06-11 22:37:26 +1000

    Add OpenTracing interceptor for standalone activities

--- contrib/temporal-opentracing/src/main/java/io/temporal/opentracing/StandardTagNames.java
+++ contrib/temporal-opentracing/src/main/java/io/temporal/opentracing/StandardTagNames.java
@@ -3,6 +3,7 @@ package io.temporal.opentracing;
 public class StandardTagNames {
   public static final String WORKFLOW_ID = "workflowId";
   public static final String RUN_ID = "runId";
+  public static final String ACTIVITY_ID = "activityId";
   public static final String PARENT_WORKFLOW_ID = "parentWorkflowId";
   public static final String PARENT_RUN_ID = "parentRunId";
 
