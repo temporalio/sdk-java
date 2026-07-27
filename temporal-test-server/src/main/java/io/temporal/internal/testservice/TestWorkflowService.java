1fb37c2d24610cd068ef1ec4b4e8843cab9a11be
Vathsala Ragireddy <34386917+vathsalaR@users.noreply.github.com>
2026-07-27 09:32:26 -0700

    update test

--- temporal-test-server/src/main/java/io/temporal/internal/testservice/TestWorkflowService.java
+++ temporal-test-server/src/main/java/io/temporal/internal/testservice/TestWorkflowService.java
@@ -1657,6 +1657,9 @@ public final class TestWorkflowService extends WorkflowServiceGrpc.WorkflowServi
       if (!r.getLinksList().isEmpty()) {
         startRequest.addAllLinks(r.getLinksList());
       }
+      if (r.hasPriority()) {
+        startRequest.setPriority(r.getPriority());
+      }
 
       StartWorkflowExecutionResponse startResult =
           startWorkflowExecutionImpl(
