479ea76c74345e99786057f5fd08534055dd7bcc
Edward Amsden <edward.amsden@temporal.io>
2026-09-04 18:09:01 -0500

    Fix WorkerFactory command-worker cleanup race (#2952)
    * Add regression test for command worker cleanup race
    
    * Prevent race by not setting workerCommandWorker to null when shutting down
    
    ---------
    
    Co-authored-by: Dan Plyukhin <dan.plyukhin@temporal.io>

--- temporal-sdk/src/main/java/io/temporal/worker/WorkerFactory.java
+++ temporal-sdk/src/main/java/io/temporal/worker/WorkerFactory.java
@@ -487,7 +487,6 @@ public final class WorkerFactory {
               }
               cache.invalidateAll();
               workflowThreadPool.shutdownNow();
-              workerCommandWorker = null;
               return null;
             })
         .whenComplete(
