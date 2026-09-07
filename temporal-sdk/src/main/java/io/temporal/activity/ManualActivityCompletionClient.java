568752449129df41dd1f69bb8767af64851865d4
Dan Plyukhin <dan.plyukhin@temporal.io>
2026-09-07 11:38:34 -0500

    Remove redundant docs

--- temporal-sdk/src/main/java/io/temporal/activity/ManualActivityCompletionClient.java
+++ temporal-sdk/src/main/java/io/temporal/activity/ManualActivityCompletionClient.java
@@ -30,12 +30,7 @@ public interface ManualActivityCompletionClient {
    * Records heartbeat for an activity
    *
    * @param details to record with the heartbeat
-   * @throws ActivityCompletionException if the server reports the activity was cancelled, reset, or
-   *     paused ({@link io.temporal.client.ActivityCanceledException}, {@link
-   *     io.temporal.client.ActivityResetException}, {@link
-   *     io.temporal.client.ActivityPausedException}), or if the heartbeat RPC fails ({@link
-   *     io.temporal.client.ActivityCompletionFailureException}, {@link
-   *     io.temporal.client.ActivityNotExistsException}).
+   * @throws ActivityCompletionException if activity should stop executing
    */
   void recordHeartbeat(@Nullable Object details) throws ActivityCompletionException;
 
