4379aa24467fef9ae560f82200520790c5d57d58
Gregory Michael Travis <greg.m.travis@gmail.com>
2026-09-08 16:25:15 -0400

    Update temporal-sdk/src/main/java/io/temporal/internal/client/ActivityHandleImpl.java
    Co-authored-by: Maciej Dudkowski <maciej.dudkowski@temporal.io>

--- temporal-sdk/src/main/java/io/temporal/internal/client/ActivityHandleImpl.java
+++ temporal-sdk/src/main/java/io/temporal/internal/client/ActivityHandleImpl.java
@@ -170,12 +170,7 @@ public final class ActivityHandleImpl implements UntypedActivityHandle {
 
   @Override
   public ActivityExecutionOptions updateOptions(ActivityOptionsUpdate<?>... updates) {
-    List<ActivityOptionsUpdate<?>> list = new ArrayList<>(updates.length);
-    for (ActivityOptionsUpdate<?> update : updates) {
-      if (update != null) {
-        list.add(update);
-      }
-    }
+    List<ActivityOptionsUpdate<?>> list = new ArrayList<>(updates);
 
     // An update naming nothing would send an empty mask and silently change nothing. Fail here
     // rather than making a round trip that looks like it worked. Use restoreOriginalOptions() to
